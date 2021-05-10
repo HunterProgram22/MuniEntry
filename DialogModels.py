dcf44eff69c81025614dfa49b328c23a3de1779d
Justin Kudela <justinkudela@gmail.com>
2021-05-10 15:28:40 -0400

    Added docxtpl and tested.

--- DialogModels.py
+++ DialogModels.py
@@ -1,3 +1,8 @@
+import sys
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docxtpl import DocxTemplate
+
 from PyQt5.QtWidgets import (
     QApplication, QDialog, QMainWindow, QMessageBox
 )
@@ -11,4 +16,9 @@ class OmnibusMotionDialog(QDialog, Ui_OmnibusMotionDialog):
         self.setupUi(self)
 
     def createEntry(self):
-        pass
+        doc = DocxTemplate("demo_template.docx")
+        context = { 'defendant_name' : 'Justin Kudela',
+                    'case_no' : '21TRD1234'
+                    }
+        doc.render(context)
+        doc.save("Demo_actual_document.docx")
