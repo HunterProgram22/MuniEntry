a304b44776f47bea8dc40b61a3b507f0b0256b67
HunterProgram22 <JustinKudela@gmail.com>
2021-09-04 21:42:00 -0400

    Updated tests.

--- tests/test_minor_misdemeanor_dialog.py
+++ tests/test_minor_misdemeanor_dialog.py
@@ -1,6 +1,7 @@
 import pytest
 import os, sys, inspect
 from time import sleep
+from keyboard import press_and_release
 
 from pytestqt.plugin import QtBot
 from PyQt5 import QtCore
@@ -51,9 +52,43 @@ def test_case_information_dialog(app, dialog):
     assert dialog.defendant_last_name_lineEdit.text() == "Smith"
 
 def test_offense_to_statute(app, dialog):
-    QtBot.keyClicks(dialog.offense_choice_box, "Speeding > 25 mph")
-    assert dialog.statute_choice_box.currentText() == "R.C.4511.21(B)(2)"
+    dialog.offense_choice_box.setCurrentText("Speeding > 25 mph")
+    assert dialog.statute_choice_box.currentText() == "R.C. 4511.21(B)(2)"
     assert dialog.degree_choice_box.currentText() == "MM"
-    QtBot.keyClicks(dialog.offense_choice_box, "Driving Under Suspension")
+    dialog.offense_choice_box.setCurrentText("Driving Under Suspension")
     assert dialog.statute_choice_box.currentText() == "R.C. 4510.11"
-    assert dialog.degree_choice_box.currentText() == "M2"
+    assert dialog.degree_choice_box.currentText() == "M1"
+
+def test_statute_to_offense(app, dialog):
+    dialog.statute_choice_box.setCurrentText("R.C. 4511.21(B)(3)")
+    assert dialog.offense_choice_box.currentText() == "Speeding > 35 mph"
+    assert dialog.degree_choice_box.currentText() == "MM"
+    dialog.statute_choice_box.setCurrentText("R.C. 4511.33")
+    assert dialog.offense_choice_box.currentText() == "Driving in Marked Lanes"
+    assert dialog.degree_choice_box.currentText() == "MM"
+
+def test_add_offense(app, dialog):
+    dialog.offense_choice_box.setCurrentText("Speeding > 25 mph")
+    dialog.plea_choice_box.setCurrentText("Not Guilty")
+    dialog.finding_choice_box.setCurrentText("Guilty")
+    dialog.fines_amount.setCurrentText("50")
+    dialog.fines_suspended.setCurrentText("25")
+    QtBot.mouseClick(dialog.addOffenseButton, QtCore.Qt.LeftButton)
+
+def test_add_multiple_offenses(app, dialog):
+    pass
+
+def test_add_offense_and_delete_offense(app, dialog):
+    pass
+
+def test_add_two_delete_one_add_one_offense(app, dialog):
+    pass
+
+def test_fines_due_date(app, dialog):
+    pass
+
+def test_amend_offense(app, dialog):
+    pass
+
+def test_creat_entry(app, dialog):
+    pass
