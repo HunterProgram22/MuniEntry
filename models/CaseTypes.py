267eaacc671ffa1ead0b61b6202bbfbaaa709756
Justin Kudela <justinkudela@gmail.com>
2021-09-22 21:36:01 -0400

    Added type hints to CaseTypes.py

--- models/CaseTypes.py
+++ models/CaseTypes.py
@@ -6,12 +6,12 @@ class Case(object):
     """The base object from which all other objects are connected. Any matter
     before the court will have at a minimum some type of case."""
 
-    def __init__(self, case_number):
+    def __init__(self, case_number: str) -> None:
         self.case_number = case_number
         self.case_type_code = case_number[2:5]
         self.case_parties = []
 
-    def add_party_to_case(self, party_type):
+    def add_party_to_case(self, party_type: str) -> None:
         """The party_type will generally be either "Plaintiff" or "Defendant."""
         self.party_type = party_type
         self.case_parties.append(self.party_type)
@@ -35,10 +35,33 @@ class TrafficCase(Case):
         out the case numbers to create a case_type_code (i.e. TRC or TRD).
         """
         super().__init__(case_number)
+        self.case_charges: list = []
+
+    def add_charge_to_case(self, traffic_charge: TrafficCharge) -> None:
+        self.traffic_charge: TrafficCharge = traffic_charge
+        self.case_charges.append(self.traffic_charge)
 
 
+class CriminalCase(Case):
+    """
+    A case type for criminal cases. The format for the case number will be
+    either YYCRBXXXX
 
-test = TrafficCase("21TRC0123")
+    YY = last two digits of the year the case was filed.
+    CRB = is for all criminal cases.
+    XXXX = the case number assigned to the case.
+
+    """
+
+    def __init__(self, case_number):
+        """
+        The initializion of a CriminalCase takes the case number and strips
+        out the case numbers to create a case_type_code CRB.
+        """
+        super().__init__(case_number)
+
+
+test = CriminalCase("21CRB0123")
 print(test.case_type_code)
 test.add_party_to_case(Party("Plaintiff"))
 print(test.case_parties)
