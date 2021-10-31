a4505897db002ec1e4abcd7f93ca64dcd16cd5f7
Justin Kudela <justinkudela@gmail.com>
2021-10-31 06:59:42 -0400

    Plyint/Flake8 pass for LPD (no tests yet)

--- controllers/leap_plea_dialogs.py
+++ controllers/leap_plea_dialogs.py
@@ -1,30 +1,14 @@
 """The controller module for the LEAP plea dialog."""
-import os
-import pathlib
-from datetime import date, timedelta
 from loguru import logger
-from docxtpl import DocxTemplate
 
-from PyQt5 import QtCore
 from PyQt5.QtCore import QDate
-from PyQt5.QtWidgets import QLabel, QPushButton, QMessageBox, QComboBox, QLineEdit
-from PyQt5.QtSql import QSqlDatabase, QSqlQuery
 
-from views.custom_widgets import (
-    PleaComboBox,
-    FindingComboBox,
-    FineLineEdit,
-    FineSuspendedLineEdit,
-    DeleteButton,
-    AmendButton,
-)
 from views.leap_plea_long_dialog_ui import Ui_LeapPleaLongDialog
 from models.template_types import TEMPLATE_DICT
-from models.case_information import CaseInformation, CriminalCharge
-from controllers.criminal_dialogs import BaseCriminalDialog
-from resources.db.DatabaseCreation import create_offense_list, create_statute_list
-from settings import LEAP_COMPLETE_DATE_DICT
+from models.case_information import CaseInformation
 from controllers.helper_functions import set_future_date
+from controllers.criminal_dialogs import BaseCriminalDialog
+from settings import LEAP_COMPLETE_DATE_DICT
 
 
 class LeapPleaLongDialog(BaseCriminalDialog, Ui_LeapPleaLongDialog):
@@ -39,39 +23,38 @@ class LeapPleaLongDialog(BaseCriminalDialog, Ui_LeapPleaLongDialog):
 
     @logger.catch
     def modify_view(self):
-        """The modify view method updates the view that is created on init."""
         super().modify_view()
         self.set_sentencing_date("120 days")
 
     @logger.catch
     def connect_signals_to_slots(self):
-        """The method connects additional signals to slots. That are not
-        included in the BaseCriminalDialog."""
         super().connect_signals_to_slots()
         self.time_to_complete_box.currentTextChanged.connect(self.set_sentencing_date)
 
     @logger.catch
     def update_case_information(self):
-        """The method calls functions to update the case information model."""
         self.update_party_information()
         self.add_dispositions_and_fines()
 
     @logger.catch
     def update_party_information(self):
         super().update_party_information()
-        self.case_information.sentencing_date = self.sentencing_date.date().toString("MMMM dd, yyyy")
+        self.case_information.sentencing_date = (
+            self.sentencing_date.date().toString("MMMM dd, yyyy")
+        )
 
     @logger.catch
     def add_dispositions_and_fines(self):
-        """Row 3 - plea.
-        Column count increases by 2 instead of one due to grid adding two
+        """Row 3 - plea. Column count increases by 2 instead of one due to grid adding two
         columns when a charge is added (odd numbered column is empty)."""
         column = 2
         try:
             for index in range(len(self.case_information.charges_list)):
-                self.case_information.charges_list[index].plea = self.charges_gridLayout.itemAtPosition(3,column).widget().currentText()
-                index +=1
-                column +=2
+                self.case_information.charges_list[index].plea = (
+                    self.charges_gridLayout.itemAtPosition(3, column).widget().currentText()
+                )
+                index += 1
+                column += 2
         except AttributeError:
             print("Attribute error allowed to pass for lack of widget")
 
