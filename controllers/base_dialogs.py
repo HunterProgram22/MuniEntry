ff3d80cfbaffaa6712f524aaed1319f203cce77a
Justin Kudela <justinkudela@gmail.com>
2021-12-11 11:22:16 -0500

    Refactored create entry and clear fields to SlotFunctions class

--- controllers/base_dialogs.py
+++ controllers/base_dialogs.py
@@ -9,12 +9,10 @@ from controllers.helper_functions import create_entry
 
 
 class CasePartyUpdater:
-    """A class containing all methods for updating case number, date and parties."""
+    """A base class containing all base methods for updating case number, date and parties."""
     @logger.catch
     def update_case_information(self):
-        """"This method may be called in multiple places when a button is pressed to
-        make sure all information is current. The subclass version of the method
-        will call updates to specific portions of the view for that dialog."""
+        """"Docstring needs updating."""
         self.set_case_number_and_date()
         self.set_party_information()
 
@@ -30,25 +28,47 @@ class CasePartyUpdater:
 
 
 class DialogCleanUp:
-    @logger.catch
-    def close_window(self):
-        """Function connected to a button to close the window. Can be connected
-        to any button press/click/release to close a window."""
-        self.close()
-
+    """A base class containing the base cleanup methods for when a dialog is completed and closed."""
     @logger.catch
     def close_event(self):
         """Place any cleanup items (i.e. close_databases) here that should be
         called when the entry is created and the dialog closed."""
         self.close_window()
 
+    @logger.catch
+    def close_window(self):
+        """Function connected to a button to close the window. Can be connected
+        to any button press/click/release to close a window. This can also be called
+        at the end of the close_event process to close the dialog."""
+        self.close()
 
-class BaseDialog(QDialog, CasePartyUpdater, DialogCleanUp):
+
+class SlotFunctions:
+    @logger.catch
+    def clear_case_information_fields(self):
+        """Clears the text in the fields in the top case information frame and resets the cursor
+        to the first text entry box."""
+        self.defendant_first_name_lineEdit.clear()
+        self.defendant_last_name_lineEdit.clear()
+        self.case_number_lineEdit.clear()
+        self.defendant_first_name_lineEdit.setFocus()
+
+    @logger.catch
+    def create_entry_process(self):
+        """The order of functions that are called when the create_entry_Button is pressed()
+        on a criminal dialog. The order is important to make sure the information is
+        updated before the entry is created."""
+        self.update_case_information()
+        if self.charges_gridLayout.check_plea_and_findings() is None:
+            return None
+        create_entry(self)
+        self.close_event()
+
+
+class BaseDialog(QDialog, CasePartyUpdater, DialogCleanUp, SlotFunctions):
     """This class is a base class to provide methods that are used by some criminal controllers
      in the application. This class is never instantiated as its own dialog, but the init contains
      the setup for all inherited class controllers."""
-
-    # Properties, vars and constructors
     def __init__(self, judicial_officer, case=None, parent=None):
         """Databases must be opened first in order for them to be accessed
         when the UI is built so it can populate fields.The setupUI calls to
@@ -78,24 +98,3 @@ class BaseDialog(QDialog, CasePartyUpdater, DialogCleanUp):
         self.cancel_Button.pressed.connect(self.close_event)
         self.clear_fields_case_Button.pressed.connect(self.clear_case_information_fields)
         self.create_entry_Button.pressed.connect(self.create_entry_process)
-
-    # Processes
-    @logger.catch
-    def clear_case_information_fields(self):
-        """Clears the text in the fields in the top case information frame and resets the cursor
-        to the first text entry box."""
-        self.defendant_first_name_lineEdit.clear()
-        self.defendant_last_name_lineEdit.clear()
-        self.case_number_lineEdit.clear()
-        self.defendant_first_name_lineEdit.setFocus()
-
-    @logger.catch
-    def create_entry_process(self):
-        """The order of functions that are called when the create_entry_Button is pressed()
-        on a criminal dialog. The order is important to make sure the information is
-        updated before the entry is created."""
-        self.update_case_information()
-        if self.charges_gridLayout.check_plea_and_findings() is None:
-            return None
-        create_entry(self)
-        self.close_event()
