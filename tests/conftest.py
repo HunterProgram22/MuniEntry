b5724bfa642f329b969c38c0fbdc5797d35bd4be
Justin Kudela <justinkudela@gmail.com>
2021-12-20 06:00:47 -0500

    Working on test_no_jail_plea_dialog

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,13 +26,6 @@ def mouse_click(button):
     return QtBot.mouseClick(button, QtCore.Qt.LeftButton)
 
 
-"""
-app = main application window
-ngbd = NotGuiltyBondDialog
-njpd = NoJailPleaDialog
-"""
-
-
 @pytest.fixture
 def app(qtbot):
     app = MuniEntry_app.Window(arraignments_database)
@@ -40,6 +33,15 @@ def app(qtbot):
     mouse_click(app.bunner_radioButton)
     enter_data(app.arraignment_cases_box, '21TRD09200')
     return app
+
+
+@pytest.fixture
+def app_nocase(qtbot):
+    app = MuniEntry_app.Window(arraignments_database)
+    qtbot.addWidget(app)
+    mouse_click(app.bunner_radioButton)
+    return app
+
 
 
 @pytest.fixture()
@@ -58,6 +60,14 @@ def njp_dialog(app, qtbot):
     return app
 
 
+@pytest.fixture()
+def njp_dialog_nocase(app_nocase, qtbot):
+    mouse_click(app_nocase.NoJailPleaButton)
+    app_nocase = NoJailPleaDialog(app_nocase.judicial_officer, app_nocase.case_to_load)
+    qtbot.addWidget(app_nocase)
+    return app_nocase
+
+
 @pytest.fixture
 def ngbd_check_special_conditions(ngb_dialog):
     mouse_click(ngb_dialog.domestic_violence_checkBox)
@@ -67,3 +77,10 @@ def ngbd_check_special_conditions(ngb_dialog):
     mouse_click(ngb_dialog.no_contact_checkBox)
     mouse_click(ngb_dialog.other_conditions_checkBox)
     mouse_click(ngb_dialog.add_special_conditions_Button)
+
+
+@pytest.fixture
+def add_case_information(njp_dialog_nocase):
+    enter_data(njp_dialog_nocase.case_number_lineEdit, "21TRC1234")
+    enter_data(njp_dialog_nocase.defendant_first_name_lineEdit, "John")
+    enter_data(njp_dialog_nocase.defendant_last_name_lineEdit, "Smith")
