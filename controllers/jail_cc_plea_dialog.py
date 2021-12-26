6effb87ed3a0257d8bf4663864308911d9c55523
Justin Kudela <justinkudela@gmail.com>
2021-12-26 07:15:03 -0500

    Moved common methods to Criminal Sentencing.

--- controllers/jail_cc_plea_dialog.py
+++ controllers/jail_cc_plea_dialog.py
@@ -13,30 +13,6 @@ class JailCCPleaDialog(CriminalSentencingDialog, Ui_JailCCPleaDialog):
         self.dialog_name = 'Jail CC Plea Dialog'
         self.template = TEMPLATE_DICT.get(self.dialog_name)
 
-    # @logger.catch
-    # def load_cms_data_to_view(self):
-    #     return CMS_FRALoader(self)
-    #
-    # @logger.catch
-    # def modify_view(self):
-    #     """Sets the balance due date in the view to today."""
-    #     super().modify_view()
-    #     self.balance_due_date.setDate(QtCore.QDate.currentDate())
-    #
-    # @logger.catch
-    # def connect_signals_to_slots(self):
-    #     """The method connects additional signals to slots. That are not
-    #     included in the BaseDialog."""
-    #     super().connect_signals_to_slots()
-    #     self.connect_plea_signals_and_slots()
-    #
-    # @logger.catch
-    # def update_case_information(self):
-    #     """"Docstring needs updating."""
-    #     super().update_case_information()
-    #     self.add_additional_case_information()
-
     def add_charge_to_grid(self):
         self.charges_gridLayout.jail_add_charge_finding_fines_and_jail_to_grid(self)
         self.statute_choice_box.setFocus()
-
