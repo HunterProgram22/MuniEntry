5c64af7a7595434c02cf6a5fea25f56ed0b95785
Justin Kudela <justinkudela@gmail.com>
2022-03-27 09:25:31 -0400

    Format only.

--- tests/test_fta_dialog.py
+++ tests/test_fta_dialog.py
@@ -8,6 +8,7 @@ from conftest import mouse_click, enter_data
 def test_dialog_opens(qtbot, dialog):
     assert dialog.windowTitle() == "Failure To Appear Case Information"
 
+
 all_fta_conditions_test_list = [
     "arrest_warrant_checkBox",
     "bond_forfeited_checkBox",
@@ -18,7 +19,6 @@ all_fta_conditions_test_list = [
     "proof_of_service_checkBox",
     "registration_block_checkBox",
 ]
-
 
 @pytest.mark.parametrize("checkBox", all_fta_conditions_test_list)
 def test_all_checkbox_conditions(qtbot, dialog, checkBox):
