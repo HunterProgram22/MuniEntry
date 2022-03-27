2b4ef7969429be723b4815c5e885b152de4e6636
Justin Kudela <justinkudela@gmail.com>
2022-03-27 05:27:27 -0400

    Moved methods around no functional change.

--- main_window.py
+++ main_window.py
@@ -3,9 +3,18 @@ from PyQt5 import QtGui
 from PyQt5.QtWidgets import QMainWindow
 
 from db.databases import create_daily_cases_list, CriminalCaseSQLRetriever
-from package.controllers.information_checkers import check_judicial_officer, check_case_list_selected
-from package.controllers.main_entry_dialogs import DiversionPleaDialog, JailCCPleaDialog, FineOnlyPleaDialog, \
-    NotGuiltyBondDialog, ProbationViolationBondDialog, FailureToAppearDialog
+from package.controllers.information_checkers import (
+    check_judicial_officer,
+    check_case_list_selected,
+)
+from package.controllers.main_entry_dialogs import (
+    DiversionPleaDialog,
+    JailCCPleaDialog,
+    FineOnlyPleaDialog,
+    NotGuiltyBondDialog,
+    ProbationViolationBondDialog,
+    FailureToAppearDialog,
+)
 from package.models.case_information import CriminalCaseInformation
 from package.models.party_types import JudicialOfficer
 from package.views.custom_widgets import ExtendedComboBox
@@ -18,12 +27,12 @@ class Window(QMainWindow, Ui_MainWindow):
         super().__init__(parent)
         self.daily_case_list_database = daily_case_list_database
         self.setupUi(self)  # The self argument that is called is MainWindow
-        self.setWindowIcon(QtGui.QIcon(f'{ICON_PATH}gavel.ico'))
+        self.setWindowIcon(QtGui.QIcon(f"{ICON_PATH}gavel.ico"))
         self.create_main_window_dicts()
         self.set_daily_case_lists_type()
         self.connect_signals_to_slots()
         self.load_case_lists()
-        self.button_state() # This is called to set boxes all to hidden b/c no button is selected on load.
+        self.button_state()  # This is called to set boxes all to hidden on load.
         self.judicial_officer = None
         self.case_to_load = None
         self.FailureToAppearButton.setHidden(False)
@@ -61,7 +70,7 @@ class Window(QMainWindow, Ui_MainWindow):
         self.daily_case_list_buttons = {
             self.arraignments_radioButton: "arraignments",
             self.slated_radioButton: "slated",
-            self.final_pretrial_radioButton:"final_pretrials",
+            self.final_pretrial_radioButton: "final_pretrials",
             self.pleas_radioButton: "pleas",
             self.trials_to_court_radioButton: "trials_to_court",
         }
@@ -91,17 +100,29 @@ class Window(QMainWindow, Ui_MainWindow):
 
     def connect_signals_to_slots(self):
         self.menu_file_exit.triggered.connect(self.close)
-        self.arraignments_radioButton.toggled.connect(lambda: self.button_state(self.arraignments_radioButton))
-        self.slated_radioButton.toggled.connect(lambda: self.button_state(self.slated_radioButton))
-        self.final_pretrial_radioButton.toggled.connect(lambda: self.button_state(self.final_pretrial_radioButton))
-        self.pleas_radioButton.toggled.connect(lambda: self.button_state(self.pleas_radioButton))
-        self.trials_to_court_radioButton.toggled.connect(lambda: self.button_state(self.trials_to_court_radioButton))
+        self.connect_daily_case_list_buttons()
         for key in self.daily_case_list_buttons:
             key.clicked.connect(self.set_case_list_table)
         for key in self.dialog_dict:
             key.pressed.connect(self.start_dialog_from_entry_button)
         for key in self.judicial_officer_dict:
             key.clicked.connect(self.set_judicial_officer)
+
+    def connect_daily_case_list_buttons(self):
+        self.arraignments_radioButton.toggled.connect(
+            lambda: self.button_state(self.arraignments_radioButton)
+        )
+        self.slated_radioButton.toggled.connect(lambda: self.button_state(self.slated_radioButton))
+        self.final_pretrial_radioButton.toggled.connect(
+            lambda: self.button_state(self.final_pretrial_radioButton)
+        )
+        self.pleas_radioButton.toggled.connect(lambda: self.button_state(self.pleas_radioButton))
+        self.trials_to_court_radioButton.toggled.connect(
+            lambda: self.button_state(self.trials_to_court_radioButton)
+        )
+
+    def set_case_list_table(self):
+        self.case_table = self.daily_case_list_buttons.get(self.sender())
 
     def button_state(self, button=None):
         if button is None:
@@ -119,22 +140,26 @@ class Window(QMainWindow, Ui_MainWindow):
                 value.setEnabled(False)
 
     def set_judicial_officer(self):
-        """Checks the judicial officer radio buttons and then sets the judicial officer to the one that is checked."""
+        """Checks the judicial officer radio buttons and then sets the judicial officer to the
+        one that is checked."""
         for key, value in self.judicial_officer_dict.items():
             if key.isChecked():
                 self.judicial_officer = value
 
-    def set_case_list_table(self):
-        self.case_table = self.daily_case_list_buttons.get(self.sender())
-
     def load_case_lists(self):
         """Loads the cms_case numbers of all the cases that are in the daily_case_list databases. This
         does not load the cms_case data for each cms_case."""
-        self.arraignment_cases_box.addItems(create_daily_cases_list("daily_case_lists.sqlite", "arraignments"))
-        self.slated_cases_box.addItems(create_daily_cases_list("daily_case_lists.sqlite","slated"))
-        self.final_pretrial_cases_box.addItems(create_daily_cases_list("daily_case_lists.sqlite","final_pretrials"))
+        self.arraignment_cases_box.addItems(
+            create_daily_cases_list("daily_case_lists.sqlite", "arraignments")
+        )
+        self.slated_cases_box.addItems(create_daily_cases_list("daily_case_lists.sqlite", "slated"))
+        self.final_pretrial_cases_box.addItems(
+            create_daily_cases_list("daily_case_lists.sqlite", "final_pretrials")
+        )
         self.pleas_cases_box.addItems(create_daily_cases_list("daily_case_lists.sqlite", "pleas"))
-        self.trials_to_court_cases_box.addItems(create_daily_cases_list("daily_case_lists.sqlite", "trials_to_court"))
+        self.trials_to_court_cases_box.addItems(
+            create_daily_cases_list("daily_case_lists.sqlite", "trials_to_court")
+        )
 
     @logger.catch
     @check_judicial_officer
@@ -146,7 +171,10 @@ class Window(QMainWindow, Ui_MainWindow):
             self.case_to_load = CriminalCaseInformation()
         else:
             case_number = selected_case_list.currentText().split("- ")[1]
-            self.case_to_load = \
-                CriminalCaseSQLRetriever(case_number, self.case_table, self.daily_case_list_database).load_case()
-        self.dialog = self.dialog_dict[self.sender()](self.judicial_officer, self.case_to_load, self.case_table)
+            self.case_to_load = CriminalCaseSQLRetriever(
+                case_number, self.case_table, self.daily_case_list_database
+            ).load_case()
+        self.dialog = self.dialog_dict[self.sender()](
+            self.judicial_officer, self.case_to_load, self.case_table
+        )
         self.dialog.exec()
