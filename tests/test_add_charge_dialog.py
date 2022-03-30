9e0145f970b37383af155944d0745a0d5b1637b8
Justin Kudela <justinkudela@gmail.com>
2022-03-29 20:42:19 -0400

    Add charge load to blank working.

--- tests/test_add_charge_dialog.py
+++ tests/test_add_charge_dialog.py
@@ -24,6 +24,12 @@ def test_add_charge_dialog_opens(add_charge_dialog):
     assert add_charge_dialog.windowTitle() == "Add Charge"
 
 
+def test_add_charge_dialog_loads_empty(add_charge_dialog):
+    assert add_charge_dialog.statute_choice_box.currentText() == ""
+    assert add_charge_dialog.offense_choice_box.currentText() == ""
+    assert add_charge_dialog.degree_choice_box.currentText() == ""
+
+
 def test_if_checking_freeform_makes_editable(add_charge_dialog):
     mouse_click(add_charge_dialog.freeform_entry_checkBox)
     assert add_charge_dialog.statute_choice_box.isEditable()
