28b811af58e31dece68a0cd7f3fe2c0366ba6fc4
Justin Kudela <justinkudela@gmail.com>
2022-03-30 05:57:43 -0400

    Updated test version number.

--- tests/test_main_window.py
+++ tests/test_main_window.py
@@ -18,7 +18,7 @@ def test_window_opens(qtbot, main_window_noclose):
     the main_window fixture causes the next test to error because the window is closed
     when it runs."""
     main_window_noclose.show()
-    assert main_window_noclose.windowTitle() == "MuniEntry - ver 0.16.0"
+    assert main_window_noclose.windowTitle() == "MuniEntry - ver 0.16.1"
 
 
 @pytest.mark.skip(reason="Requires manual interaction")
