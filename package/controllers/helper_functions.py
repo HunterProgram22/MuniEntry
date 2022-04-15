6082be05e2d78a0d3a79640eabf42755b054a45c
Justin Kudela <justinkudela@gmail.com>
2022-04-15 16:59:03 -0400

    Adding type hints and working on Vim usage.

--- package/controllers/helper_functions.py
+++ package/controllers/helper_functions.py
@@ -2,13 +2,8 @@
 from datetime import date, timedelta
 
 
-def set_future_date(days_to_add, weekday_due_date):
-    """This is a general helper function that will accept the numbers of days to
-    add to the current date and set a new date.
-
-    :days_to_add: A string from the view.
-
-    :weekday_due_date: The next day of the week to set the date after the days to add.
+def set_future_date(days_to_add: int = None, weekday_due_date: int = 0)-> QDate:
+    """weekday_due_date: The next day of the week to set the date after the days to add.
     For example 0 days sets it to the next Monday after the added days, 1 sets
     it to the next Tuesday after the added days, etc."""
     future_date = date.today() + timedelta(days_to_add)
@@ -18,7 +13,7 @@ def set_future_date(days_to_add, weekday_due_date):
     return total_days_to_add
 
 
-def next_court_day(future_date, weekday_due_date):
+def next_court_day(future_date: QDate, weekday_due_date: int = 0):
     """This function returns the number of days to add to today to set
     the actual date needed. If it is 1 it would be Tuesday, 3 would
     be Wednesday, etc."""
