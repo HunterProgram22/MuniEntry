ff68424275ea1650464a03249892fe0f7f7ba3de
Justin Kudela <justinkudela@gmail.com>
2022-05-07 10:37:57 -0400

    Working on refactoring to single transfer field/view to model in based

--- package/controllers/case_updaters.py
+++ package/controllers/case_updaters.py
@@ -103,9 +103,9 @@ class DiversionDialogCaseModelUpdater(CaseModelUpdater):
         self.update_case_information()
 
     def update_case_information(self):
-        self.transfer_view_data_to_model(self.model.diversion)
+        self.view.transfer_field_data_to_model(self.model.diversion)
         self.model.diversion.program_name = self.model.diversion.get_program_name()
-        self.transfer_view_data_to_model(self.model.other_conditions)
+        self.view.transfer_field_data_to_model(self.model.other_conditions)
 
     def update_model_with_charge_grid_data(self):
         return DiversionGridModelUpdater(self.view, self.model)
@@ -231,7 +231,7 @@ class NoPleaBondDialogCaseModelUpdater(CaseModelUpdater):
         self.update_bond_conditions()
 
     def update_bond_conditions(self):
-        self.transfer_view_data_to_model(self.model.bond_conditions)
+        self.view.transfer_field_data_to_model(self.model.bond_conditions)
 
 
 class BondHearingDialogCaseModelUpdater(CaseModelUpdater):
