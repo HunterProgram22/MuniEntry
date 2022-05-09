ea69f1cb482de0e0fb415ca698cc53984e96955a
Justin Kudela <justinkudela@gmail.com>
2022-05-09 16:45:21 -0400

    Refactored base with Wemake to fit personal style.

--- package/controllers/base_dialogs.py
+++ package/controllers/base_dialogs.py
@@ -5,11 +5,7 @@ from settings import WIDGET_TYPE_ACCESS_DICT
 
 
 class BaseDialog(QDialog):
-    """This class is a base class for all dialog windows.
-
-    Every window must have a view loaded (modify view), slot functions created, and then the slot
-    functions connected to signals.
-    """
+    """This class is a base class for all dialog windows."""
 
     def __init__(self, parent: QDialog = None) -> None:
         super().__init__(parent)
@@ -18,48 +14,22 @@ class BaseDialog(QDialog):
         self.connect_signals_to_slots()
 
     def modify_view(self):
-        """This method calls the self.setupUI method to setup the UI, then runs other file setup.
-
-        Place items in this returned class that can't be added directly in QtDesigner (or are
-        more easily added later) so that they don't need to be changed in the view file each
-        time pyuic5 is run.
-
-        Raises:
-            NotImplementedError: When subclass does not contain method.
-        """
         raise NotImplementedError
 
     def create_dialog_slot_functions(self):
-        """Calls class that contains all functions for the dialog.
-
-        Each dialog has functions tied to certain slots (i.e. buttons, checkboxes, etc.) the
-        functions are placed in a class and returned to the main dialog so that they can be
-        connected to the signals.
-
-        Raises:
-            NotImplementedError: When subclass does not contain method.
-        """
         raise NotImplementedError
 
     def connect_signals_to_slots(self):
-        """Calls class that connects all signals in dialog to functions.
-
-        Each dialog has its own signal connector class that connects the signals to the
-        slot functions class for that dialog.
-
-        Raises:
-            NotImplementedError: When subclass does not contain method.
-        """
         raise NotImplementedError
 
     def transfer_view_data_to_model(self, model_class: object) -> None:
         """Takes data in the view fields and transfers to appropriate model class attribute.
 
-        Loops through a model's terms list to transfer data from the view to the model using
-        the appropriate method for the view widget.
+        Method loops through all items in terms list which are maps of a model attribute to
+        a view field. The appropriate transfer method is obtained from the WIDGET_TYPE_ACCESS_DICT
 
         Args:
-            model_class: A dataclass object that has terms_list attribute mapping
+            model_class: A dataclass object that has a terms_list attribute mapping
                 view fields to model attributes.
         """
         for (model_attribute, view_field) in model_class.terms_list:
