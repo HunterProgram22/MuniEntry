d443f62ce9576fe286d515babc03324000cd44af
Justin Kudela <justinkudela@gmail.com>
2022-05-15 07:00:37 -0400

    Moved Version number to settings.

--- MuniEntry_app.py
+++ MuniEntry_app.py
@@ -8,9 +8,10 @@ import sys
 
 from loguru import logger
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QSplashScreen
 
-from settings import ICON_PATH
+from settings import ICON_PATH, VERSION_NUMBER
 
 logger.add('./resources/logs/Error_log_{time}.log')
 
@@ -24,14 +25,13 @@ def load_window():
 
 @logger.catch
 def main():
-    print('Loading')
     app = QApplication(sys.argv)
     splash = QSplashScreen(QPixmap(f'{ICON_PATH}gavel.png'))
     splash.show()
-    splash.showMessage('Loading')
+    splash.showMessage(f'<h1>Loading - Version {VERSION_NUMBER}</h1>', Qt.AlignBottom | Qt.AlignCenter)
     win = load_window()
     win.show()
-    splash.showMessage('Databases Connected')
+    splash.showMessage('<h1>Databases Connected</h1>', Qt.AlignBottom | Qt.AlignCenter)
     splash.close()
     sys.exit(app.exec())
 
