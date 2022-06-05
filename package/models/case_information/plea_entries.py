0c6df1c82b7a479adc6c33b12e922b99741bd3c3
Justin Kudela <justinkudela@gmail.com>
2022-06-05 18:02:57 -0400

    Still working on plea_entries.

--- package/models/case_information/plea_entries.py
+++ package/models/case_information/plea_entries.py
@@ -1,32 +1,21 @@
 """Contains all dataclasses for entries that do not contain FRA or Court Costs."""
 from dataclasses import dataclass, field
 
+from package.models import conditions_models as cm
 from package.models.case_information.criminal_case_information import (
     CriminalCaseInformation,
-)
-from package.models.conditions_models import (
-    AdminLicenseSuspensionConditions,
-    BondConditions,
-    CommunityControlViolationBondConditions,
-    CustodialSupervision,
-    DomesticViolenceBondConditions,
-    FailureToAppearConditions,
-    FutureSentencing,
-    NoContact,
-    OtherConditions,
-    VehicleSeizure,
 )
 
 
 @dataclass
 class NotGuiltyBondEntryCaseInformation(CriminalCaseInformation):
-    bond_conditions: object = field(default_factory=BondConditions)
-    no_contact: object = field(default_factory=NoContact)
-    custodial_supervision: object = field(default_factory=CustodialSupervision)
-    domestic_violence_conditions: object = field(default_factory=DomesticViolenceBondConditions)
-    admin_license_suspension: object = field(default_factory=AdminLicenseSuspensionConditions)
-    vehicle_seizure: object = field(default_factory=VehicleSeizure)
-    other_conditions: object = field(default_factory=OtherConditions)
+    bond_conditions: object = field(default_factory=cm.BondConditions)
+    no_contact: object = field(default_factory=cm.NoContact)
+    custodial_supervision: object = field(default_factory=cm.CustodialSupervision)
+    domestic_violence_conditions: object = field(default_factory=cm.DomesticViolenceBondConditions)
+    admin_license_suspension: object = field(default_factory=cm.AdminLicenseSuspensionConditions)
+    vehicle_seizure: object = field(default_factory=cm.VehicleSeizure)
+    other_conditions: object = field(default_factory=cm.OtherConditions)
 
 
 @dataclass
@@ -41,18 +30,18 @@ class NoPleaBondEntryCaseInformation(NotGuiltyBondEntryCaseInformation):
 
 @dataclass
 class PleaOnlyEntryCaseInformation(CriminalCaseInformation):
-    future_sentencing: object = field(default_factory=FutureSentencing)
+    future_sentencing: object = field(default_factory=cm.FutureSentencing)
 
 
 @dataclass
 class CommunityControlViolationEntryCaseInformation(CriminalCaseInformation):
     cc_violation_probable_cause: str = None
-    cc_bond_conditions: object = field(default_factory=CommunityControlViolationBondConditions)
+    cc_bond_conditions: object = field(default_factory=cm.CommunityControlViolationBondConditions)
 
 
 @dataclass
 class FailureToAppearEntryCaseInformation(CriminalCaseInformation):
-    fta_conditions: object = field(default_factory=FailureToAppearConditions)
+    fta_conditions: object = field(default_factory=cm.FailureToAppearConditions)
 
 
 @dataclass
