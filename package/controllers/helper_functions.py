40093ed00407bf0e9738fc4d7d3829c40dd4c398
Justin Kudela <justinkudela@gmail.com>
2022-06-20 06:02:49 -0400

    Cleaned up helper_functions.py

--- package/controllers/helper_functions.py
+++ package/controllers/helper_functions.py
@@ -3,8 +3,11 @@ from datetime import date, timedelta
 
 
 def set_future_date(days_to_add: int, weekday_due_date: str) -> int:
-    """Accepts the number of days to add and a day of the week that will be added on to the
-    number of days for the future court date that is to be set."""
+    """Adds days to a date and sets a future weekday date.
+
+    Accepts the number of days to add and a day of the week that will be added on to the
+    number of days for the future court date that is to be set.
+    """
     today = date.today()
     future_date = today + timedelta(days_to_add)
     future_date = next_court_day(future_date, weekday_due_date)
@@ -12,14 +15,13 @@ def set_future_date(days_to_add: int, weekday_due_date: str) -> int:
 
 
 def next_court_day(future_date: date, weekday_due_date: str) -> date:
-    """Returns a date object that is the total number of days plus the days required to be
-    added to get to the next specified court date"""
+    """Returns a date object that is the total number of days plus the days required to be added."""
     weekday_convert_dict = {
-        "Monday": 0,
-        "Tuesday": 1,
-        "Wednesday": 2,
-        "Thursday": 3,
-        "Friday": 4,
+        'Monday': 0,
+        'Tuesday': 1,
+        'Wednesday': 2,
+        'Thursday': 3,
+        'Friday': 4,
     }
     weekday = weekday_convert_dict.get(weekday_due_date, 0)
     days_ahead = weekday - future_date.weekday()
