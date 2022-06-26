8ea0cd3f79c4e40bb74708bbd4a138727783009f
Justin Kudela <justinkudela@gmail.com>
2022-06-26 06:50:34 -0400

    Added 2 tests for new hearing notices - passing.

--- tests/test_scheduling_entry.py
+++ tests/test_scheduling_entry.py
@@ -16,9 +16,33 @@ def sch_dialog_hemmeter(qtbot, main_window):
     return main_window.dialog
 
 
+@pytest.fixture
+def jury_final_hearing_notice(qtbot, main_window):
+    mouse_click(main_window.arraignments_radioButton)
+    mouse_click(main_window.rohrer_radioButton)
+    mouse_click(main_window.notice_of_hearingEntryButton)
+    return main_window.dialog
+
+
+@pytest.fixture
+def trial_to_court_hearing_notice(qtbot, main_window):
+    mouse_click(main_window.arraignments_radioButton)
+    mouse_click(main_window.hemmeter_radioButton)
+    mouse_click(main_window.trial_to_court_hearingEntryButton)
+    return main_window.dialog
+
+
 def test_scheduing_entry_rohrer_opens(qtbot, sch_dialog_rohrer):
-    assert sch_dialog_rohrer.windowTitle() == "Rohrer scheduling Entry Case Information"
+    assert sch_dialog_rohrer.windowTitle() == "Rohrer Scheduling Entry Case Information"
+
 
 def test_scheduing_entry_hemmeter_opens(qtbot, sch_dialog_hemmeter):
-    assert sch_dialog_hemmeter.windowTitle() == "Hemmeter scheduling Entry Case Information"
+    assert sch_dialog_hemmeter.windowTitle() == "Hemmeter Scheduling Entry Case Information"
 
+
+def test_jury_final_hearing_notice_opens(qtbot, jury_final_hearing_notice):
+    assert jury_final_hearing_notice.windowTitle() == "Notice Of Hearing Entry Rohrer Case Information"
+
+
+def test_trial_to_court_hearing_notice_opens(qtbot, trial_to_court_hearing_notice):
+    assert trial_to_court_hearing_notice.windowTitle() == "Trial To Court Hearing Notice Case Information"
