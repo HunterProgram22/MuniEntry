2b0d47f6d83c2a030efee3c165bc7ebc0ebdf6de
Justin Kudela <justinkudela@gmail.com>
2022-06-30 15:49:52 -0400

    Flake8 and pylint on bond_dialogs.

--- package/dialog_builders/bond_dialogs.py
+++ package/dialog_builders/bond_dialogs.py
@@ -1,26 +1,54 @@
+"""Module for building main entry bond dialogs."""
+from loguru import logger
+
 from package.controllers.cms_case_loaders import CmsNoChargeLoader
-from package.controllers.signal_connectors import NoPleaBondDialogSignalConnector, \
-    ProbationViolationBondDialogSignalConnector, BondHearingDialogSignalConnector
-from package.controllers.slot_functions import NoPleaBondDialogSlotFunctions, \
-    ProbationViolationBondDialogSlotFunctions, BondHearingDialogSlotFunctions
-from package.controllers.view_modifiers import NoPleaBondDialogViewModifier, \
-    ProbationViolationBondDialogViewModifier, BondHearingDialogViewModifier
+from package.controllers.signal_connectors import (
+    BondHearingDialogSignalConnector,
+    NoPleaBondDialogSignalConnector,
+    ProbationViolationBondDialogSignalConnector,
+)
+from package.controllers.slot_functions import (
+    BondHearingDialogSlotFunctions,
+    NoPleaBondDialogSlotFunctions,
+    ProbationViolationBondDialogSlotFunctions,
+)
+from package.controllers.view_modifiers import (
+    BondHearingDialogViewModifier,
+    NoPleaBondDialogViewModifier,
+    ProbationViolationBondDialogViewModifier,
+)
 from package.dialog_builders.base_dialogs import CriminalBaseDialog
-from package.information_checkers.bond_checkers import NoPleaBondDialogInfoChecker, \
-    ProbationViolationBondDialogInfoChecker, BondHearingDialogInfoChecker
-from package.models.case_information.plea_entries import NoPleaBondEntryCaseInformation, \
-    CommunityControlViolationEntryCaseInformation, BondHearingEntryCaseInformation
-from package.models.conditions_models import BondConditions, \
-    CommunityControlViolationBondConditions, BondModificationConditions
+from package.information_checkers.bond_checkers import (
+    BondHearingDialogInfoChecker,
+    NoPleaBondDialogInfoChecker,
+    ProbationViolationBondDialogInfoChecker,
+)
+from package.models.case_information.plea_entries import (
+    BondHearingEntryCaseInformation,
+    CommunityControlViolationEntryCaseInformation,
+    NoPleaBondEntryCaseInformation,
+)
+from package.models.conditions_models import (
+    BondConditions,
+    BondModificationConditions,
+    CommunityControlViolationBondConditions,
+)
 from package.models.template_types import TEMPLATE_DICT
-from package.updaters.no_grid_case_updaters import NoPleaBondDialogUpdater, \
-    ProbationViolationBondDialogUpdater, BondHearingDialogUpdater
+from package.updaters.no_grid_case_updaters import (
+    BondHearingDialogUpdater,
+    NoPleaBondDialogUpdater,
+    ProbationViolationBondDialogUpdater,
+)
 from package.views.bond_hearing_dialog_ui import Ui_BondHearingDialog
 from package.views.no_plea_bond_dialog_ui import Ui_NoPleaBondDialog
-from package.views.probation_violation_bond_dialog_ui import Ui_ProbationViolationBondDialog
+from package.views.probation_violation_bond_dialog_ui import (
+    Ui_ProbationViolationBondDialog,
+)
 
 
 class NoPleaBondDialog(CriminalBaseDialog, Ui_NoPleaBondDialog):
+    """Dialog builder class for 'Appear on Warrant (No Plea) / Bond' Entry."""
+
     condition_checkbox_dict = {
         'monitoring_checkBox': ['monitoring_type_box'],
         'specialized_docket_checkBox': ['specialized_docket_type_box'],
@@ -56,7 +84,7 @@ class NoPleaBondDialog(CriminalBaseDialog, Ui_NoPleaBondDialog):
         return NoPleaBondDialogSignalConnector(self)
 
     def update_entry_case_information(self):
-        '''Calls the dialog specific CaseModelUpdater in the grid_case_updaters.py module.'''
+        """Calls the dialog specific CaseModelUpdater in the grid_case_updaters.py module."""
         return NoPleaBondDialogUpdater(self)
 
     def load_entry_case_information_model(self):
@@ -71,6 +99,8 @@ class NoPleaBondDialog(CriminalBaseDialog, Ui_NoPleaBondDialog):
 
 
 class ProbationViolationBondDialog(CriminalBaseDialog, Ui_ProbationViolationBondDialog):
+    """Dialog builder class for 'Prelim. Probation Violation / Bond' Entry."""
+
     def __init__(self, judicial_officer, cms_case=None, case_table=None, parent=None):
         super().__init__(judicial_officer, cms_case, case_table, parent)
         self.dialog_name = 'Probation Violation Bond Dialog'
@@ -102,6 +132,8 @@ class ProbationViolationBondDialog(CriminalBaseDialog, Ui_ProbationViolationBond
 
 
 class BondHearingDialog(CriminalBaseDialog, Ui_BondHearingDialog):
+    """Dialog builder class for 'Bond Modification / Revocation' Entry."""
+
     condition_checkbox_dict = {
         'monitoring_checkBox': ['monitoring_type_box'],
         'specialized_docket_checkBox': ['specialized_docket_type_box'],
@@ -150,8 +182,14 @@ class BondHearingDialog(CriminalBaseDialog, Ui_BondHearingDialog):
         return CmsNoChargeLoader(self)
 
     def update_entry_case_information(self) -> BondHearingDialogUpdater:
-        '''Calls the dialog specific CaseModelUpdater in the grid_case_updaters.py module.'''
+        """Calls the dialog specific CaseModelUpdater in the grid_case_updaters.py module."""
         return BondHearingDialogUpdater(self)
 
     def perform_info_checks(self) -> None:
         self.dialog_checks = BondHearingDialogInfoChecker(self)
+
+
+if __name__ == '__main__':
+    logger.log('IMPORT', f'{__name__} run directly.')
+else:
+    logger.log('IMPORT', f'{__name__} imported.')
