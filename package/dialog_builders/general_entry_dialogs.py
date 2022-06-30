93309d03c26dc9f4ebdca648ab7b731ee9b76431
Justin Kudela <justinkudela@gmail.com>
2022-06-30 16:32:23 -0400

    Flake8 and pylint on general_entry_dialogs.

--- package/dialog_builders/general_entry_dialogs.py
+++ package/dialog_builders/general_entry_dialogs.py
@@ -1,10 +1,7 @@
 """The module that contains the main classes for creating an entry dialog."""
 from loguru import logger
 
-from package.dialog_builders.base_dialogs import CriminalBaseDialog
-from package.controllers.cms_case_loaders import (
-    CmsNoChargeLoader,
-)
+from package.controllers.cms_case_loaders import CmsNoChargeLoader
 from package.controllers.signal_connectors import (
     FailureToAppearDialogSignalConnector,
     FreeformDialogSignalConnector,
@@ -17,6 +14,7 @@ from package.controllers.view_modifiers import (
     FailureToAppearDialogViewModifier,
     FreeformDialogViewModifier,
 )
+from package.dialog_builders.base_dialogs import CriminalBaseDialog
 from package.information_checkers.base_checks import (
     FailureToAppearDialogInfoChecker,
     FreeformDialogInfoChecker,
@@ -25,9 +23,7 @@ from package.models.case_information.plea_entries import (
     FailureToAppearEntryCaseInformation,
     FreeformEntryCaseInformation,
 )
-from package.models.conditions_models import (
-    FailureToAppearConditions,
-)
+from package.models.conditions_models import FailureToAppearConditions
 from package.models.template_types import TEMPLATE_DICT
 from package.updaters.no_grid_case_updaters import (
     FailureToAppearDialogUpdater,
@@ -38,18 +34,23 @@ from package.views.freeform_dialog_ui import Ui_FreeformEntryDialog
 
 
 class FailureToAppearDialog(CriminalBaseDialog, Ui_FailureToAppearDialog):
+    """Dialog builder class for 'Failure To Appear / Issue Warrant' Entry."""
+
     def __init__(self, judicial_officer, cms_case=None, case_table=None, parent=None):
         super().__init__(judicial_officer, cms_case, case_table, parent)
         self.dialog_name = 'Failure To Appear Dialog'
         self.template = TEMPLATE_DICT.get(self.dialog_name)
         self.entry_case_information.fta_conditions = FailureToAppearConditions()
-        if (
-            self.case_table == 'final_pretrials'
-        ):  # Ovverides the set_appearance_reason in ViewModifier
-            self.appearance_reason_box.setCurrentText('final pre-trial')
 
     def modify_view(self):
-        return FailureToAppearDialogViewModifier(self)
+        """Standard call to build the view, then updates appearance reason.
+
+        Sets appearance reason to final pre-trial instead of change of plea if dialog opens a
+        case from the final pretrials table.
+        """
+        FailureToAppearDialogViewModifier(self)
+        if self.case_table == 'final_pretrials':
+            self.appearance_reason_box.setCurrentText('final pre-trial')
 
     def create_dialog_slot_functions(self):
         self.functions = FailureToAppearDialogSlotFunctions(self)
@@ -74,6 +75,8 @@ class FailureToAppearDialog(CriminalBaseDialog, Ui_FailureToAppearDialog):
 
 
 class FreeformDialog(CriminalBaseDialog, Ui_FreeformEntryDialog):
+    """Dialog builder class for 'Freeform Entry'."""
+
     def __init__(self, judicial_officer, cms_case=None, case_table=None, parent=None):
         super().__init__(judicial_officer, cms_case, case_table, parent)
         self.dialog_name = 'Freeform Entry Dialog'
