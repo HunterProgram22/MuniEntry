1c80a7d0ff1c7b5abac10dcb0b62dac1be77025c
Justin Kudela <justinkudela@gmail.com>
2022-06-30 20:30:25 -0400

    Mypy on bond_dialogs.

--- package/dialog_builders/bond_dialogs.py
+++ package/dialog_builders/bond_dialogs.py
@@ -54,7 +54,13 @@ class NoPleaBondDialog(CriminalBaseDialog, Ui_NoPleaBondDialog):
         'specialized_docket_checkBox': ['specialized_docket_type_box'],
     }
 
-    def __init__(self, judicial_officer, cms_case=None, case_table=None, parent=None):
+    def __init__(
+        self,
+        judicial_officer: object,
+        cms_case: str = None,
+        case_table: str = None,
+        parent: object = None,
+    ) -> None:
         super().__init__(judicial_officer, cms_case, case_table, parent)
         self.additional_conditions_list = [
             (
@@ -74,60 +80,65 @@ class NoPleaBondDialog(CriminalBaseDialog, Ui_NoPleaBondDialog):
         self.template = TEMPLATE_DICT.get(self.dialog_name)
         self.entry_case_information.bond_conditions = BondConditions()
 
-    def modify_view(self):
+    def modify_view(self) -> NoPleaBondDialogViewModifier:
         return NoPleaBondDialogViewModifier(self)
 
-    def create_dialog_slot_functions(self):
+    def create_dialog_slot_functions(self) -> None:
         self.functions = NoPleaBondDialogSlotFunctions(self)
 
-    def connect_signals_to_slots(self):
+    def connect_signals_to_slots(self) -> NoPleaBondDialogSignalConnector:
         return NoPleaBondDialogSignalConnector(self)
 
-    def update_entry_case_information(self):
-        """Calls the dialog specific CaseModelUpdater in the grid_case_updaters.py module."""
+    def update_entry_case_information(self) -> NoPleaBondDialogUpdater:
         return NoPleaBondDialogUpdater(self)
 
-    def load_entry_case_information_model(self):
+    def load_entry_case_information_model(self) -> None:
         self.entry_case_information = NoPleaBondEntryCaseInformation()
         self.entry_case_information.judicial_officer = self.judicial_officer
 
-    def load_cms_data_to_view(self):
+    def load_cms_data_to_view(self) -> CmsNoChargeLoader:
         return CmsNoChargeLoader(self)
 
-    def perform_info_checks(self):
+    def perform_info_checks(self) -> None:
         self.dialog_checks = NoPleaBondDialogInfoChecker(self)
 
 
 class ProbationViolationBondDialog(CriminalBaseDialog, Ui_ProbationViolationBondDialog):
     """Dialog builder class for 'Prelim. Probation Violation / Bond' Entry."""
 
-    def __init__(self, judicial_officer, cms_case=None, case_table=None, parent=None):
+    def __init__(
+        self,
+        judicial_officer: object,
+        cms_case: str = None,
+        case_table: str = None,
+        parent: object = None,
+    ) -> None:
         super().__init__(judicial_officer, cms_case, case_table, parent)
         self.dialog_name = 'Probation Violation Bond Dialog'
         self.template = TEMPLATE_DICT.get(self.dialog_name)
         self.entry_case_information.bond_conditions = CommunityControlViolationBondConditions()
 
-    def modify_view(self):
+    def modify_view(self) -> ProbationViolationBondDialogViewModifier:
         return ProbationViolationBondDialogViewModifier(self)
 
-    def create_dialog_slot_functions(self):
+    def create_dialog_slot_functions(self) -> None:
         self.functions = ProbationViolationBondDialogSlotFunctions(self)
         self.functions.hide_bond_conditions()
 
-    def connect_signals_to_slots(self):
+    def connect_signals_to_slots(self) -> ProbationViolationBondDialogSignalConnector:
         return ProbationViolationBondDialogSignalConnector(self)
 
-    def load_entry_case_information_model(self):
+    def load_entry_case_information_model(self) -> None:
         self.entry_case_information = CommunityControlViolationEntryCaseInformation()
         self.entry_case_information.judicial_officer = self.judicial_officer
 
-    def load_cms_data_to_view(self):
+    def load_cms_data_to_view(self) -> CmsNoChargeLoader:
         return CmsNoChargeLoader(self)
 
-    def update_entry_case_information(self):
+    def update_entry_case_information(self) -> ProbationViolationBondDialogUpdater:
         return ProbationViolationBondDialogUpdater(self)
 
-    def perform_info_checks(self):
+    def perform_info_checks(self) -> None:
         self.dialog_checks = ProbationViolationBondDialogInfoChecker(self)
 
 
@@ -182,7 +193,6 @@ class BondHearingDialog(CriminalBaseDialog, Ui_BondHearingDialog):
         return CmsNoChargeLoader(self)
 
     def update_entry_case_information(self) -> BondHearingDialogUpdater:
-        """Calls the dialog specific CaseModelUpdater in the grid_case_updaters.py module."""
         return BondHearingDialogUpdater(self)
 
     def perform_info_checks(self) -> None:
