6de775027cc15618d855f4337d711739ccfa8db8
Justin Kudela <justinkudela@gmail.com>
2022-07-01 05:15:21 -0400

    Flake8, mypy and pylint on plea_sentence_dialogs.

--- package/dialog_builders/plea_sentence_dialogs.py
+++ package/dialog_builders/plea_sentence_dialogs.py
@@ -1,27 +1,56 @@
+"""Module containing classes to build Plea and Sentencing Dialogs."""
 from PyQt5.QtGui import QIntValidator
+
 from package.controllers.cms_case_loaders import CmsFraLoader
-from package.controllers.signal_connectors import DiversionDialogSignalConnector, \
-    JailCCDialogSignalConnector, FineOnlyDialogSignalConnector
-from package.controllers.slot_functions import DiversionDialogSlotFunctions, \
-    JailCCDialogSlotFunctions, FineOnlyDialogSlotFunctions
-from package.controllers.view_modifiers import DiversionDialogViewModifier, \
-    JailCCDialogViewModifier, FineOnlyDialogViewModifier
+from package.controllers.signal_connectors import (
+    DiversionDialogSignalConnector,
+    FineOnlyDialogSignalConnector,
+    JailCCDialogSignalConnector,
+)
+from package.controllers.slot_functions import (
+    DiversionDialogSlotFunctions,
+    FineOnlyDialogSlotFunctions,
+    JailCCDialogSlotFunctions,
+)
+from package.controllers.view_modifiers import (
+    DiversionDialogViewModifier,
+    FineOnlyDialogViewModifier,
+    JailCCDialogViewModifier,
+)
 from package.dialog_builders.base_dialogs import CriminalBaseDialog
-from package.information_checkers.jail_charge_grid_checkers import JailCCPleaDialogInfoChecker
-from package.information_checkers.no_jail_sentencing_checkers import DiversionDialogInfoChecker, \
-    FineOnlyDialogInfoChecker
-from package.models.case_information.sentencing_entries import DiversionEntryCaseInformation, \
-    JailCCEntryCaseInformation, FineOnlyEntryCaseInformation
+from package.information_checkers.jail_charge_grid_checkers import (
+    JailCCPleaDialogInfoChecker,
+)
+from package.information_checkers.no_jail_sentencing_checkers import (
+    DiversionDialogInfoChecker,
+    FineOnlyDialogInfoChecker,
+)
+from package.models.case_information.sentencing_entries import (
+    DiversionEntryCaseInformation,
+    FineOnlyEntryCaseInformation,
+    JailCCEntryCaseInformation,
+)
 from package.models.template_types import TEMPLATE_DICT
-from package.updaters.grid_case_updaters import DiversionDialogUpdater, JailCCDialogUpdater, \
-    FineOnlyDialogUpdater
+from package.updaters.grid_case_updaters import (
+    DiversionDialogUpdater,
+    FineOnlyDialogUpdater,
+    JailCCDialogUpdater,
+)
 from package.views.diversion_plea_dialog_ui import Ui_DiversionPleaDialog
 from package.views.fine_only_plea_dialog_ui import Ui_FineOnlyPleaDialog
 from package.views.jail_cc_plea_dialog_ui import Ui_JailCCPleaDialog
 
 
 class DiversionPleaDialog(CriminalBaseDialog, Ui_DiversionPleaDialog):
-    def __init__(self, judicial_officer, cms_case=None, case_table=None, parent=None):
+    """Dialog builder class for 'Diversion' dialog."""
+
+    def __init__(
+        self,
+        judicial_officer: object,
+        cms_case: str = None,
+        case_table: str = None,
+        parent: object = None,
+    ) -> None:
         super().__init__(judicial_officer, cms_case, case_table, parent)
         self.dialog_name = 'Diversion Plea Dialog'
         self.template = TEMPLATE_DICT.get(self.dialog_name)
@@ -30,15 +59,15 @@ class DiversionPleaDialog(CriminalBaseDialog, Ui_DiversionPleaDialog):
     def modify_view(self) -> None:
         DiversionDialogViewModifier(self)
 
-    def create_dialog_slot_functions(self):
+    def create_dialog_slot_functions(self) -> None:
         self.functions = DiversionDialogSlotFunctions(self)
         self.functions.show_jail_report_date_box()
         self.functions.show_other_conditions_box()
 
-    def connect_signals_to_slots(self):
-        return DiversionDialogSignalConnector(self)
+    def connect_signals_to_slots(self) -> None:
+        DiversionDialogSignalConnector(self)
 
-    def load_entry_case_information_model(self):
+    def load_entry_case_information_model(self) -> None:
         self.entry_case_information = DiversionEntryCaseInformation()
         self.entry_case_information.judicial_officer = self.judicial_officer
         self.entry_case_information.diversion.ordered = True
@@ -46,15 +75,23 @@ class DiversionPleaDialog(CriminalBaseDialog, Ui_DiversionPleaDialog):
     def load_cms_data_to_view(self) -> None:
         CmsFraLoader(self)
 
-    def update_entry_case_information(self):
-        return DiversionDialogUpdater(self)
+    def update_entry_case_information(self) -> None:
+        DiversionDialogUpdater(self)
 
-    def perform_info_checks(self):
+    def perform_info_checks(self) -> None:
         self.dialog_checks = DiversionDialogInfoChecker(self)
 
 
 class JailCCPleaDialog(CriminalBaseDialog, Ui_JailCCPleaDialog):
-    def __init__(self, judicial_officer, cms_case=None, case_table=None, parent=None):
+    """Dialog builder class for 'Jail and/or Community Control' dialog."""
+
+    def __init__(
+        self,
+        judicial_officer: object,
+        cms_case: str = None,
+        case_table: str = None,
+        parent: object = None,
+    ) -> None:
         super().__init__(judicial_officer, cms_case, case_table, parent)
         self.validator = QIntValidator(0, 1000, self)
         self.jail_time_credit_box.setValidator(self.validator)
@@ -75,29 +112,37 @@ class JailCCPleaDialog(CriminalBaseDialog, Ui_JailCCPleaDialog):
     def modify_view(self) -> None:
         JailCCDialogViewModifier(self)
 
-    def create_dialog_slot_functions(self):
+    def create_dialog_slot_functions(self) -> None:
         self.functions = JailCCDialogSlotFunctions(self)
         self.functions.show_companion_case_fields()
 
-    def connect_signals_to_slots(self):
-        return JailCCDialogSignalConnector(self)
+    def connect_signals_to_slots(self) -> None:
+        JailCCDialogSignalConnector(self)
 
-    def load_entry_case_information_model(self):
+    def load_entry_case_information_model(self) -> None:
         self.entry_case_information = JailCCEntryCaseInformation()
         self.entry_case_information.judicial_officer = self.judicial_officer
 
     def load_cms_data_to_view(self) -> None:
         CmsFraLoader(self)
 
-    def update_entry_case_information(self):
-        return JailCCDialogUpdater(self)
+    def update_entry_case_information(self) -> None:
+        JailCCDialogUpdater(self)
 
-    def perform_info_checks(self):
+    def perform_info_checks(self) -> None:
         self.dialog_checks = JailCCPleaDialogInfoChecker(self)
 
 
 class FineOnlyPleaDialog(CriminalBaseDialog, Ui_FineOnlyPleaDialog):
-    def __init__(self, judicial_officer, cms_case=None, case_table=None, parent=None):
+    """Dialog builder class for 'Fine Only' dialog."""
+
+    def __init__(
+        self,
+        judicial_officer: object,
+        cms_case: str = None,
+        case_table: str = None,
+        parent: object = None,
+    ) -> None:
         super().__init__(judicial_officer, cms_case, case_table, parent)
         self.additional_conditions_list = [
             ('license_suspension_checkBox', self.entry_case_information.license_suspension),
@@ -110,22 +155,22 @@ class FineOnlyPleaDialog(CriminalBaseDialog, Ui_FineOnlyPleaDialog):
     def modify_view(self) -> None:
         FineOnlyDialogViewModifier(self)
 
-    def create_dialog_slot_functions(self):
+    def create_dialog_slot_functions(self) -> None:
         self.functions = FineOnlyDialogSlotFunctions(self)
         self.functions.set_fines_credit_for_jail_field()
 
-    def connect_signals_to_slots(self):
-        return FineOnlyDialogSignalConnector(self)
+    def connect_signals_to_slots(self) -> None:
+        FineOnlyDialogSignalConnector(self)
 
-    def load_entry_case_information_model(self):
+    def load_entry_case_information_model(self) -> None:
         self.entry_case_information = FineOnlyEntryCaseInformation()
         self.entry_case_information.judicial_officer = self.judicial_officer
 
     def load_cms_data_to_view(self) -> None:
         CmsFraLoader(self)
 
-    def update_entry_case_information(self):
-        return FineOnlyDialogUpdater(self)
+    def update_entry_case_information(self) -> None:
+        FineOnlyDialogUpdater(self)
 
-    def perform_info_checks(self):
+    def perform_info_checks(self) -> None:
         self.dialog_checks = FineOnlyDialogInfoChecker(self)
