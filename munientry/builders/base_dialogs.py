dd27ff180af1a0ee9025adb1e9c9416f9efe4541
Justin Kudela <justinkudela@gmail.com>
2022-07-10 06:06:12 -0400

    Added public safety suspension to 3 bond dialogs and templates.

--- munientry/builders/base_dialogs.py
+++ munientry/builders/base_dialogs.py
@@ -45,10 +45,6 @@ class BaseDialog(QDialog):
             key = getattr(self, view_field).__class__.__name__
             view = getattr(self, view_field)
             view_field_data = getattr(view, WIDGET_TYPE_ACCESS_DICT.get(key, 'None'))()
-            if view_field_data == '':
-                continue
-            if view_field_data is False:
-                continue
             setattr(model_class, model_attribute, view_field_data)
             logger.info(f'{model_class.__class__.__name__} {model_attribute} set to:'
                         + f' {view_field_data}.')
