6404c01c8e05bad469dd683fd3cc46e58d3bb828
Justin Kudela <justinkudela@gmail.com>
2022-08-01 05:57:58 -0400

    Still cleaning up sql_lite_functions.

--- munientry/data/sql_lite_functions.py
+++ munientry/data/sql_lite_functions.py
@@ -13,48 +13,73 @@ from munientry.data.sql_lite_queries import (
 from munientry.settings import DB_PATH, EXCEL_DAILY_CASE_LISTS
 
 
-def load_daily_case_list_data(con_daily_case_lists: QSqlDatabase) -> None:
+def load_daily_case_list_data(database: QSqlDatabase) -> None:
     """Loads case data from SSRS reports in Excel into the application.
+
+    :database: The SQL Lite database object for MuniEntryDB.sqlite.
 
     The function loops through the six SSRS Excel reports for the daily case lists and loads the
     case data in those files into the application SQL Lite internal database.
     """
-    for item in EXCEL_DAILY_CASE_LISTS:
-        excel_report, table_name = item
-        delete_existing_sql_table_data(con_daily_case_lists, table_name)
-        insert_daily_case_list_sql_data(con_daily_case_lists, excel_report, table_name)
+    for ssrs_report in EXCEL_DAILY_CASE_LISTS:
+        excel_report, table_name = ssrs_report
+        delete_existing_sql_table_data(database, table_name)
+        insert_daily_case_list_sql_data(database, excel_report, table_name)
 
 
 def insert_daily_case_list_sql_data(
-    con_daily_case_lists: QSqlDatabase, excel_report: str, table_name: str
+    database: QSqlDatabase, excel_report: str, table_name: str,
 ) -> None:
-    cases_from_table = return_cases_data_from_excel(f'{DB_PATH}{excel_report}')
-    insert_data_query = QSqlQuery(con_daily_case_lists)
-    insert_data_query.prepare(insert_daily_case_list_tables_sql_query(table_name))
-    for case in cases_from_table:
-        insert_data_query.addBindValue(case.case_number)
-        insert_data_query.addBindValue(case.defendant_last_name)
-        insert_data_query.addBindValue(case.defendant_first_name)
-        insert_data_query.addBindValue(case.offense)
-        insert_data_query.addBindValue(case.statute)
-        insert_data_query.addBindValue(case.degree)
-        insert_data_query.addBindValue(case.fra_in_file)
-        insert_data_query.addBindValue(case.moving_bool)
-        insert_data_query.addBindValue(case.def_atty_last_name)
-        insert_data_query.addBindValue(case.def_atty_first_name)
-        insert_data_query.addBindValue(case.def_atty_type)
-        insert_data_query.exec()
+    """Inserts data from each row of SSRS report as a case into SQL Lite database.
+
+    :database: The SQL Lite database object for MuniEntryDB.sqlite.
+
+    :excel_report: The name of the SSRS report (excel file). The path is added in the function.
+
+    :table_name: The name of the SSRS report that corresponds to the table in the SQL Lite
+        database.
+    """
+    cases_list = return_cases_data_from_excel(f'{DB_PATH}{excel_report}')
+    query = QSqlQuery(database)
+    query.prepare(insert_daily_case_list_tables_sql_query(table_name))
+    for case in cases_list:
+        query.addBindValue(case.case_number)
+        query.addBindValue(case.defendant_last_name)
+        query.addBindValue(case.defendant_first_name)
+        query.addBindValue(case.offense)
+        query.addBindValue(case.statute)
+        query.addBindValue(case.degree)
+        query.addBindValue(case.fra_in_file)
+        query.addBindValue(case.moving_bool)
+        query.addBindValue(case.def_atty_last_name)
+        query.addBindValue(case.def_atty_first_name)
+        query.addBindValue(case.def_atty_type)
+        query.exec()
 
 
-def delete_existing_sql_table_data(db_connection: QSqlDatabase, table_name: str) -> None:
-    delete_table = QSqlQuery(db_connection)
-    delete_table.prepare(delete_table_sql_query(table_name))
-    delete_table.exec()
+def delete_existing_sql_table_data(database: QSqlDatabase, table_name: str) -> None:
+    """Function for deleting the data from a table, but not deleting the table itself.
+
+    :database: The SQL Lite database object for MuniEntryDB.sqlite.
+
+    :table_name: The name of the SSRS report that corresponds to the table in the SQL Lite
+        database.
+    """
+    query = QSqlQuery(database)
+    query.prepare(delete_table_sql_query(table_name))
+    query.exec()
 
 
-def query_offense_statute_data(db_connection: QSqlDatabase, query_value: str) -> list:
+def query_offense_statute_data(database: QSqlDatabase, query_value: str) -> list:
+    """Function for getting the offense or statute from the charges table.
+
+    :database: The SQL Lite database object for MuniEntryDB.sqlite.
+
+    :query_value: A string provided by the application based on the field being queried that
+        will be either 'offense' or 'statute.'
+    """
     query_string = select_distinct_offense_statute_sql_query()
-    query = QSqlQuery(db_connection)
+    query = QSqlQuery(database)
     query.prepare(query_string)
     query.exec()
     item_list = []
