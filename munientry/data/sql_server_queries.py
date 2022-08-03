b1175d0725cf7b60718dc372d042894467397c3c
Justin Kudela <justinkudela@gmail.com>
2022-08-03 05:19:24 -0400

    Flake8, pylint and mypy passing sql_server_queries.py

--- munientry/data/sql_server_queries.py
+++ munientry/data/sql_server_queries.py
@@ -1,6 +1,7 @@
 """Module containing all SQL Server queries used throughout the application."""
 from loguru import logger
 
+
 def general_case_search_query(case_number: str) -> str:
     return f"""
     SELECT
@@ -14,14 +15,18 @@ def general_case_search_query(case_number: str) -> str:
     CasePerson.FirstName as DefFirstName,
     CasePerson.MiddleName as DefMiddleName,
     CasePerson.Suffix as DefSuffix
-
     FROM [AuthorityCourt].[dbo].[CaseMaster]
-    LEFT OUTER JOIN [AuthorityCourt].[dbo].[SubCase] 
+    LEFT OUTER JOIN [AuthorityCourt].[dbo].[SubCase]
     ON CaseMaster.Id = SubCase.CaseMasterID
-    LEFT OUTER JOIN [AuthorityCourt].[dbo].[Violation] 
+    LEFT OUTER JOIN [AuthorityCourt].[dbo].[Violation]
     ON SubCase.ViolationId = Violation.Id
-    LEFT OUTER JOIN [AuthorityCourt].[dbo].[CasePerson] 
+    LEFT OUTER JOIN [AuthorityCourt].[dbo].[CasePerson]
     ON CasePerson.CaseMasterID = SubCase.CaseMasterID and CasePerson.PersonTypeID = '1'
-    
     WHERE CaseNumber = '{case_number}'
     """
+
+
+if __name__ == '__main__':
+    logger.log('IMPORT', f'{__name__} run directly.')
+else:
+    logger.log('IMPORT', f'{__name__} imported.')
