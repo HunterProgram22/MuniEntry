03706a892317fc58dd332cf5aa322cee9688c4fc
Justin Kudela <justinkudela@gmail.com>
2022-11-13 10:03:35 -0500

    Updated if name is main for connections.py to allow Sphinx to build.

--- munientry/loaders/driving_caseload_functions.py
+++ munientry/loaders/driving_caseload_functions.py
@@ -10,8 +10,10 @@ def load_no_case_driving():
 
     Avoids unnecessary call to the database when there is no data to load.
 
-    :return: A DrivingPrivilegesInformation object with no data.
-    :rtype: DrivingPrivilegesInformation
+    Returns
+    -------
+    DrivingPrivilegesInformation
+        The DrivingPrivilegesInformation model with no data loaded.
     """
     return DrivingPrivilegesInformation()
 
@@ -19,11 +21,16 @@ def load_no_case_driving():
 def load_single_driving_info_case(case_number: str):
     """Loads a single case with Driving Info query into the CmsCaseInformation model.
 
-    :param case_number: A string of the case number in the format: [2 digits][3 chars][5 digits].
+    Parameters
+    ----------
+    case_number: str
+        The case number to load in the format: [2 digits][3 chars][5 digits].
         Example: 22TRD12345, 21CRB00001, 20TRD01010
-    :type case_number: str
-    :return: A DrivingPrivilegesInformation object with case data from the database.
-    :rtype: DrivingPrivilegesInformation
+
+    Returns
+    -------
+    DrivingPrivilegesInformation
+        A DrivingPrivilegesInformation object with case data from the database.
     """
     return sql_server.DrivingInfoSQLServer(case_number).load_case()
 
