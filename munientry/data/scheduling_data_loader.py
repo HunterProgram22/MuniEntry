42ff8910c60bed2c0be6c1507bfa7dfe2f391bee
Justin Kudela <justinkudela@gmail.com>
2022-11-15 20:20:04 -0500

    Date saving properly as string in correct format.

--- munientry/data/scheduling_data_loader.py
+++ munientry/data/scheduling_data_loader.py
@@ -1,4 +1,6 @@
 """Module for extracting scheduling data and loading into the MuniEntry Database."""
+from datetime import datetime
+
 from loguru import logger
 from PyQt6.QtSql import QSqlDatabase, QSqlQuery
 
@@ -21,7 +23,15 @@ def save_scheduling_data(case_data):
        """
     )
     query.addBindValue(case_data.get('case_number'))
-    query.addBindValue(case_data.get('final_pretrial_date'))
+    date_string = case_data.get('final_pretrial_date')
+    date_object = datetime.strptime(date_string, '%B %d, %Y')
+    logger.info(date_object)
+    new_date_format = date_object.strftime('%Y-%m-%d')
+    logger.info(new_date_format)
+    new_date_object = datetime.strptime(new_date_format, '%Y-%m-%d').date()
+    logger.info(new_date_object)
+    new_date_object = str(new_date_object)
+    query.addBindValue(new_date_object)
     query.addBindValue(case_data.get('final_pretrial_time'))
     query.exec()
     close_db_connection(conn)
