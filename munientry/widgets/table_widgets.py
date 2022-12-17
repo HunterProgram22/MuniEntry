2583a77badf8e6cdbc27e905915c62de235b7765
Justin Kudela <justinkudela@gmail.com>
2022-12-17 09:58:55 -0500

    Flake8 and pylint table_widgets.

--- munientry/widgets/table_widgets.py
+++ munientry/widgets/table_widgets.py
@@ -14,6 +14,8 @@ Classes:
     ReportTable(QTableWidget)
 
 """
+from collections import namedtuple
+
 from loguru import logger
 from PyQt6 import QtGui, QtPrintSupport
 from PyQt6.QtWidgets import (
@@ -26,10 +28,38 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
-from munientry.appsettings.paths import ICON_PATH
+from munientry.appsettings.paths import GAVEL_PATH
+
+TABLE_HEIGHT = 1000
+TABLE_WIDTH = 800
+WINDOW_HEIGHT = 1000
+WINDOW_WIDTH = 800
 
 
 class ReportTable(QTableWidget):
+    """Custom table widget for displaying information.
+
+    Args:
+        rows (int): The number of rows for the table.
+
+        cols (int): The number of columns for the table.
+
+        title (str): The name of the table.
+
+        parent (obj): The parent widget.
+
+    Attrs:
+        rows (int): The number of rows for the table.
+
+        cols (int): The number of columns for the table.
+
+        title (str): The name of the table.
+
+        parent (obj): The parent widget.
+
+        header (obj): The header row that contains column names.
+    """
+
     def __init__(self, rows, cols, title, parent=None):
         super().__init__(rows, cols, parent)
         self.title = title
@@ -37,7 +67,7 @@ class ReportTable(QTableWidget):
         logger.info(self.title)
 
     def set_up_widget(self):
-        self.setWindowIcon(QtGui.QIcon(ICON_PATH + 'gavel.ico'))
+        self.setWindowIcon(QtGui.QIcon(GAVEL_PATH))
         self.setWindowTitle(self.title)
         self.setSizeAdjustPolicy(QAbstractScrollArea.SizeAdjustPolicy.AdjustToContents)
         self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
@@ -46,7 +76,7 @@ class ReportTable(QTableWidget):
         self.header.setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
         self.setSortingEnabled(False)
         self.setAlternatingRowColors(True)
-        self.resize(1000, 800)
+        self.resize(TABLE_HEIGHT, TABLE_WIDTH)
 
 
 class ReportWindow(QWidget):
@@ -63,7 +93,7 @@ class ReportWindow(QWidget):
         super().__init__()
         self.report_name = report_name
         self.setWindowTitle(self.tr(self.report_name))
-        self.setWindowIcon(QtGui.QIcon(ICON_PATH + 'gavel.ico'))
+        self.setWindowIcon(QtGui.QIcon(GAVEL_PATH))
 
 
 class TableReportWindow(ReportWindow):
@@ -79,6 +109,7 @@ class TableReportWindow(ReportWindow):
     def __init__(self, report_name):
         super().__init__(report_name)
         self.table = None
+        self.clipboard = QtGui.QGuiApplication.clipboard()
         self.print_button = QPushButton('Print', self)
         self.print_button.clicked.connect(self.handle_print)
         self.preview_button = QPushButton('Preview', self)
@@ -89,7 +120,7 @@ class TableReportWindow(ReportWindow):
         self.layout.addWidget(self.print_button, 1, 0)
         self.layout.addWidget(self.preview_button, 1, 1)
         self.layout.addWidget(self.copy_button, 1, 2)
-        self.resize(1000, 800)
+        self.resize(WINDOW_HEIGHT, WINDOW_WIDTH)
 
     def add_table(self, rows: int, cols: int, report_name: str, parent: object) -> ReportTable:
         table = ReportTable(rows, cols, report_name, parent)
@@ -97,18 +128,29 @@ class TableReportWindow(ReportWindow):
         return table
 
     def handle_copy(self):
-        self.clipboard = QtGui.QGuiApplication.clipboard()
-        selected = self.table.selectedRanges()
+        selected = self.get_selected_text()
         copied_text = ''
-        for row in range(selected[0].topRow(), selected[0].bottomRow() + 1):
-            for col in range(selected[0].leftColumn(), selected[0].rightColumn() + 1):
+        for row in range(selected.start_row, selected.end_row):
+            for col in range(selected.start_col, selected.end_col):
                 try:
-                    copied_text += str(self.table.item(row, col).text()) + "\t"
+                    text = str(self.table.item(row, col).text())
                 except AttributeError:
-                    copied_text += "\t"
-            copied_text = copied_text[:-1] + "\n"  # eliminate last '\t'
+                    text = ''
+                copied_text = f'{copied_text} {text}\t'
+            copied_text = copied_text[:-1]  # Removes last tab
+            copied_text = f'{copied_text}\n'
         logger.info(copied_text)
         self.clipboard.setText(copied_text)
+
+    def get_selected_text(self) -> namedtuple:
+        """Returns row and column ranges for selected text in a namedtuple."""
+        selected = self.table.selectedRanges()
+        start_row = selected[0].topRow()
+        end_row = selected[0].bottomRow() + 1
+        start_col = selected[0].leftColumn()
+        end_col = selected[0].rightColumn() + 1
+        Selection = namedtuple('Selection', 'start_row end_row start_col end_col')
+        return Selection(start_row, end_row, start_col, end_col)
 
     def handle_print(self):
         printer = QtPrintSupport.QPrinter()
@@ -118,8 +160,8 @@ class TableReportWindow(ReportWindow):
 
     def handle_preview(self):
         dialog = QtPrintSupport.QPrintPreviewDialog()
-        dialog.setWindowIcon(QtGui.QIcon(ICON_PATH + 'gavel.ico'))
-        dialog.resize(1000, 800)
+        dialog.setWindowIcon(QtGui.QIcon(GAVEL_PATH))
+        dialog.resize(WINDOW_HEIGHT, WINDOW_WIDTH)
         dialog.paintRequested.connect(self.handle_paint_request)
         dialog.exec()
 
@@ -127,15 +169,13 @@ class TableReportWindow(ReportWindow):
         document = QtGui.QTextDocument()
         cursor = QtGui.QTextCursor(document)
         h1_style = '{text-align: center; font-family: "Palatino Linotype", Palatino, serif;}'
-        title = cursor.insertHtml(f'''
-            <style>
-                h1 {h1_style}
-            </style>
+        cursor.insertHtml(
+            f"""
+            <style>h1 {h1_style}</style>
             <h1>DELAWARE MUNICIPAL COURT</h1>
-            <br>
-            <h3>{self.report_name}</h3>
-           <hr> 
-            ''')
+            <br><h3>{self.report_name}</h3><hr>
+            """,
+        )
         if self.table.rowCount() > 0:
             table = cursor.insertTable(self.table.rowCount(), self.table.columnCount())
             for row in range(table.rows()):
