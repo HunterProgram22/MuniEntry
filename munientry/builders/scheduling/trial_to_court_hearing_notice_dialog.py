d494c1885b3e5f15510e58378ab2a9230c8ec3a7
Justin Kudela <justinkudela@gmail.com>
2022-12-31 05:35:16 -0500

    Speedy trial date for trial to court working.

--- munientry/builders/scheduling/trial_to_court_hearing_notice_dialog.py
+++ munientry/builders/scheduling/trial_to_court_hearing_notice_dialog.py
@@ -10,9 +10,13 @@ from munientry.appsettings.pyqt_constants import TODAY
 from munientry.updaters.scheduling_updaters import (
     SchedulingDialogCaseInformationUpdater,
 )
+from munientry.appsettings.business_constants import DAY_DICT, SPEEDY_TRIAL_TIME_DICT
 from munientry.views.trial_to_court_hearing_dialog_ui import (
     Ui_TrialToCourtHearingDialog,
 )
+
+TRIAL = 'Trial'
+ENTRY_DATE_FORMAT = 'MMMM dd, yyyy'
 
 
 class TrialToCourtDialogViewModifier(sched.SchedulingViewModifier):
@@ -30,9 +34,47 @@ class TrialToCourtDialogViewModifier(sched.SchedulingViewModifier):
 class TrialToCourtDialogSlotFunctions(sched.SchedulingSlotFunctions):
     """Class for Trial To Court Hearing Notice Functions - only inherits at present."""
 
+    def set_event_date(self, day_to_set: str, event_to_set: str) -> 'QDate':
+        days_until_speedy_trial_date = TODAY.daysTo(self.get_speedy_trial_date())
+        event_date = TODAY.addDays(days_until_speedy_trial_date)
+        while event_date.dayOfWeek() != DAY_DICT.get(day_to_set):
+            event_date = event_date.addDays(-1)
+        return event_date
+
     def update_all_scheduled_dates(self):
-            trial_date = self.set_event_date('Tuesday', TRIAL)
+            trial_date = self.set_event_date('Monday', TRIAL)
             self.dialog.trial_dateEdit.setDate(trial_date)
+
+    def get_speedy_trial_date(self) -> 'QDate':
+        speedy_trial_days = self.get_speedy_trial_days()
+        days_in_jail = self.get_days_in_jail()
+        continuance_days = self.get_continuance_days()
+        speedy_trial_days = (speedy_trial_days + continuance_days) - days_in_jail
+        return self.dialog.arrest_summons_date_box.date().addDays(speedy_trial_days)
+
+    def set_speedy_trial_date_label(self):
+        speedy_trial_date = self.get_speedy_trial_date()
+        speedy_trial_date = speedy_trial_date.toString(ENTRY_DATE_FORMAT)
+        self.dialog.speedy_trial_date_label.setText(speedy_trial_date)
+
+    def get_speedy_trial_days(self) -> int:
+        key = self.dialog.highest_charge_box.currentText()
+        return SPEEDY_TRIAL_TIME_DICT.get(key)
+
+    def get_days_in_jail(self) -> int:
+        """Multiply days in jail times 3 for speedy trial calculations."""
+        if self.dialog.days_in_jail_lineEdit.text() == '':
+            days_in_jail = 0
+        else:
+            days_in_jail = int(self.dialog.days_in_jail_lineEdit.text())
+        return 3 * days_in_jail
+
+    def get_continuance_days(self) -> int:
+        if self.dialog.continuance_days_lineEdit.text() == '':
+            continuance_days = 0
+        else:
+            continuance_days = int(self.dialog.continuance_days_lineEdit.text())
+        return continuance_days
 
 
 class TrialToCourtDialogSignalConnector(sched.SchedulingSignalConnector):
