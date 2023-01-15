ba4ec4358879eb2d5d4a2fec94f7813278e218e7
Justin Kudela <justinkudela@gmail.com>
2023-01-15 07:36:46 -0500

    Flake8, pylint and mypy batch package.

--- munientry/menu/batch/batch.py
+++ munientry/menu/batch/batch.py
@@ -1,8 +1,10 @@
-import os
+"""Module for running batch processes."""
+from os import startfile
 
 from loguru import logger
+
 from munientry.appsettings.paths import BATCH_SAVE_PATH
-from munientry.mainwindow.batch_entries import run_batch_fta_arraignments
+from munientry.menu.batch.batch_entries import run_batch_fta_arraignments
 from munientry.widgets import message_boxes
 
 
@@ -11,5 +13,5 @@ def run_batch_fta_process(_signal=None) -> None:
     entries_created = run_batch_fta_arraignments()
     message = f'The batch process created {entries_created} entries.'
     message_boxes.InfoBox(message, 'Entries Created').exec()
-    os.startfile(f'{BATCH_SAVE_PATH}')
+    startfile(f'{BATCH_SAVE_PATH}')
     logger.info(f'{message}')
