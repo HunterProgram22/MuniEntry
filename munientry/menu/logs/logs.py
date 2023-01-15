fdd1a8142d6e99ffcfd03bcf2b7dac30e32a0e5c
Justin Kudela <justinkudela@gmail.com>
2023-01-15 07:42:01 -0500

    Flake8, pylint and mypy logs package.

--- munientry/menu/logs/logs.py
+++ munientry/menu/logs/logs.py
@@ -1,11 +1,13 @@
-import os
+"""Module for menu log processes."""
+from os import startfile
 
 from loguru import logger
+
 from munientry.appsettings.paths import LOG_PATH
 from munientry.logging_module import USER_LOG_NAME
 
 
 def open_current_log(_signal=None) -> None:
     """Menu function that opens the user logs directly or with keyboard shortcut."""
-    os.startfile(f'{LOG_PATH}{USER_LOG_NAME}')
+    startfile(f'{LOG_PATH}{USER_LOG_NAME}')
     logger.info(f'Current system log opened.')
