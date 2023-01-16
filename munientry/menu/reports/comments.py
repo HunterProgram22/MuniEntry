f0b7164e9264bb15ab32845e0e26acc20bff5423
Justin Kudela <justinkudela@gmail.com>
2023-01-16 05:35:15 -0500

    Added FinalPretrialComments writer.

--- munientry/menu/reports/comments.py
+++ munientry/menu/reports/comments.py
@@ -3,13 +3,15 @@
 def get_comment_writer(event):
     if event == 'Arraignments':
         return ArraignmentComments()
+    elif event == 'Final Pretrials':
+        return FinalPretrialComments()
 
 
 class ArraignmentComments(object):
 
     def get_comment(self, query) -> str:
         event = query.value('EventID')
-        judge = query.value('JudgeID')  # Judge Rohrer = 31, Judge Hemmeter = 42
+        judge = query.value('JudgeID')
         match (event, judge):
             case (27, 0) | (28, 0):
                 return 'Arraignment'
@@ -21,6 +23,25 @@ class ArraignmentComments(object):
                 return 'Unclassified Possible Data Error in Case'
 
 
+class FinalPretrialComments(object):
+
+    def get_comment(self, query) -> str:
+        event = query.value('EventID')
+        judge = query.value('JudgeID')
+        match (event, judge):
+            case (160, 42):  # Final pretrial for Judge H listed in Courtroom A
+                return 'Possible Data Issue - Judge Hemmeter assigned, FPT in CMI is set for Courtroom A'
+            case (161, 42):  # Final pretrial for Judge H listed in Courtroom B
+                return 'Courtroom B'
+            case (334, 42) | (335, 42) | (336, 42) | (337, 42):  # Pretrial/ALS code likely should be Final pretrial code
+                return 'Possible Data Issue - Event in CMI is Pretrial/ALS - Should likely be FPTN2B'
+            case (160, 31):  # Final pretrial for Judge R listed in Courtroom A
+                return 'Courtroom A'
+            case (161, 31):  # Final pretrial for Judge R listed in Courtroom B
+                return 'Possible Data Issue - Judge Rohrer assigned, FPT in CMI is set for Courtroom B'
+            case _:
+                return 'Unclassified Possible Data Error in Case'
+
 
 
 
@@ -28,16 +49,7 @@ def get_comment_field(query) -> str:
     event = query.value('EventID')
     judge = query.value('JudgeID')  # Judge Rohrer = 31, Judge Hemmeter = 42
     match (event, judge):
-        case (160, 42):  # Final pretrial for Judge H listed in Courtroom A
-            return 'Possible Data Issue - Judge Hemmeter assigned, FPT in CMI is set for Courtroom A'
-        case (161, 42):  # Final pretrial for Judge H listed in Courtroom B
-            return 'Courtroom B'
-        case (334, 42) | (335, 42) | (336, 42) | (337, 42):  # Pretrial/ALS code likely should be Final pretrial code
-            return 'Possible Data Issue - Event in CMI is Pretrial/ALS - Should likely be FPTN2B'
-        case (160, 31):  # Final pretrial for Judge R listed in Courtroom A
-            return 'Courtroom A'
-        case (161, 31):  # Final pretrial for Judge R listed in Courtroom B
-            return 'Possible Data Issue - Judge Rohrer assigned, FPT in CMI is set for Courtroom B'
+
         case (334, 31) | (335, 31) | (336, 31) | (337 | 31):  # Pretrial/ALS code likely should be Final pretrial code
             return 'Possible Data Issue - Event in CMI is Pretrial/ALS - Should likely be FPTN2'
         case (414, 31) | (414, 42):  # Trial to Court in C with Judge Assigned
@@ -50,8 +62,6 @@ def get_comment_field(query) -> str:
             return 'Courtroom B - Trial to Court with Judge Hemmeter'
         case (160, 0) | (161, 0):
             return 'No Judge Assigned to Case'
-        case (27, 0) | (28, 0):
-            return 'Arraignment'
         case (292, 31):  # Plea for Judge Rohrer listed in Courtroom A
             return 'Courtroom A'
         case (293, 31):  # Plea for Judge Rohrer listed in Courtroom B
@@ -64,9 +74,5 @@ def get_comment_field(query) -> str:
             return 'Possible Data Issue - Plea in CMI is set for Courtroom C and a Judge is Assigned'
         case (294, 0):  # Plea for no judge listed in Courtroom C
             return 'Plea in C - No Judge Assigned'
-        case (77, 0):
-            return 'Arraignment - Previously Continued'
-        case (361, 0):
-            return 'Arraignment - Reset due to FTA or Other Reason'
         case _:
             return 'Unclassified Possible Data Error in Case'
