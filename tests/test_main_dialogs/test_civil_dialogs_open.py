faa52132fbb6b36ad1d6edb8f8f1785f907e6a22
Justin Kudela <justinkudela@gmail.com>
2023-01-28 18:03:48 -0500

    Updated tests.

--- tests/test_main_dialogs/test_civil_dialogs_open.py
+++ tests/test_main_dialogs/test_civil_dialogs_open.py
@@ -13,7 +13,7 @@ admin_dialog_buttons = [
 
 @pytest.mark.parametrize(TEST_LIST, admin_dialog_buttons)
 def tests_dialogs_open_nocase_from_caselist(main_window, dialog_button, dialog_title):
-    """Tests all Admin dialog buttons open from the case list when no case is selected."""
+    """Tests all Civil dialog buttons open from the case list when no case is selected."""
     # mouse_click(main_window.assn_comm_patterson_radioButton)
     # mouse_click(main_window.arraignments_radioButton)
     mouse_click(getattr(main_window, dialog_button))
@@ -32,15 +32,15 @@ def tests_dialogs_open_nocase_from_caselist(main_window, dialog_button, dialog_t
 #     assert main_window.dialog.case_number_lineEdit.text() == '21TRC05611'
 #
 #
-# @pytest.mark.parametrize(TEST_LIST, admin_dialog_buttons)
-# def test_dialogs_open_nocase_from_casesearch(main_window, dialog_button, dialog_title):
-#     """Tests all Admin dialog buttons open from the case search when no case is selected."""
-#     mouse_click(main_window.assn_comm_patterson_radioButton)
-#     main_window.search_tabWidget.setCurrentWidget(main_window.case_search_tab)
-#     mouse_click(getattr(main_window, dialog_button))
-#     assert main_window.dialog.windowTitle() == dialog_title
-#     assert main_window.dialog.case_number_lineEdit.text() == ''
-#
+@pytest.mark.parametrize(TEST_LIST, admin_dialog_buttons)
+def test_dialogs_open_nocase_from_casesearch(main_window, dialog_button, dialog_title):
+    """Tests all Admin dialog buttons open from the case search when no case is selected."""
+    # mouse_click(main_window.assn_comm_patterson_radioButton)
+    main_window.search_tabWidget.setCurrentWidget(main_window.civil_case_search_tab)
+    mouse_click(getattr(main_window, dialog_button))
+    assert main_window.dialog.windowTitle() == dialog_title
+    assert main_window.dialog.case_number_lineEdit.text() == ''
+
 #
 # @pytest.mark.parametrize(TEST_LIST, admin_dialog_buttons)
 # def test_dialogs_open_with_case_from_casesearch(main_window, dialog_button, dialog_title):
