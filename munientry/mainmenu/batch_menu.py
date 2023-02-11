c4eb810ae41a63e6dfae99bf9137b9d0403ddd5b
Justin Kudela <justinkudela@gmail.com>
2023-02-11 12:12:40 -0500

    Refactoring batch menu with Chat gpt.

--- munientry/mainmenu/batch_menu.py
+++ munientry/mainmenu/batch_menu.py
@@ -1,29 +1,32 @@
 """Module for creating a batch of Failure to Appear entries."""
 import datetime
 from os import startfile
-from typing import List, Tuple
+from typing import TYPE_CHECKING, List, Tuple
 
-from PyQt6.QtSql import QSqlQuery
-from PyQt6.QtWidgets import QInputDialog
 from docxtpl import DocxTemplate
 from loguru import logger
+from PyQt6.QtSql import QSqlQuery
+from PyQt6.QtWidgets import QInputDialog
 
+from munientry.appsettings.paths import BATCH_SAVE_PATH, TEMPLATE_PATH
 from munientry.data.connections import close_db_connection, open_db_connection
-from munientry.appsettings.paths import BATCH_SAVE_PATH, DB_PATH, TEMPLATE_PATH
-from munientry.sqlserver.sql_server_queries import batch_fta_query, general_case_search_query
 from munientry.sqlserver.sql_server_getters import CriminalCaseSqlServer
+from munientry.sqlserver.sql_server_queries import batch_fta_query
 from munientry.widgets import message_boxes
+
+if TYPE_CHECKING:
+    from PyQt6.QtWidgets import QMainWindow
 
 
 def create_entry(case_data, event_date):
     """General create entry function that populates a template with data."""
     doc = DocxTemplate(fr'{TEMPLATE_PATH}\Batch_Failure_To_Appear_Arraignment_Template.docx')
     data_dict = {
-        'case_number':case_data.case.case_number,
+        'case_number': case_data.case.case_number,
         'def_first_name': case_data.case.defendant.first_name,
         'def_last_name': case_data.case.defendant.last_name,
         'case_event_date': event_date,
-        'warrant_rule': set_warrant_rule(case_data.case.case_number[2:5])
+        'warrant_rule': set_warrant_rule(case_data.case.case_number[2:5]),
     }
     doc.render(data_dict)
     docname = set_document_name(case_data.case_number)
@@ -35,13 +38,13 @@ def set_document_name(case_number: str) -> str:
     return f'{case_number}_FTA_Arraignment.docx'
 
 
-def prompt_user_for_batch_date(mainwindow: 'QMainWindow') -> Tuple[str, bool]:
+def prompt_user_for_batch_date(mainwindow: QMainWindow) -> Tuple[str, bool]:
     """Prompts the user for a date and returns the date and a response indicating."""
     return QInputDialog.getText(
         mainwindow,
         'Batch FTA Entries',
         'Enter the Arraignment Date in format YYYY-MM-DD:',
-        )
+    )
 
 
 def set_warrant_rule(case_type_code: str) -> str:
@@ -51,7 +54,8 @@ def set_warrant_rule(case_type_code: str) -> str:
     return 'Traffic Rule 7'
 
 
-def get_fta_arraignment_cases(query_string) -> List[str]:
+def get_fta_arraignment_cases(query_string: str) -> List[str]:
+    """Queries AuthorityCourtDB to get all cases that need a FTA warrant."""
     db_conn = open_db_connection('con_authority_court')
     query = QSqlQuery(db_conn)
     query.prepare(query_string)
@@ -63,13 +67,15 @@ def get_fta_arraignment_cases(query_string) -> List[str]:
     return data_list
 
 
-def add_one_day_to_date_string(date_string, date_format='%Y-%m-%d'):
+def add_one_day_to_date_string(date_string: str, date_format='%Y-%m-%d') -> str:
+    """Returns a string date one day later than the date string submitted."""
     date = datetime.datetime.strptime(date_string, date_format)
     date = date + datetime.timedelta(days=1)
     return date.strftime(date_format)
 
 
-def create_fta_entries(batch_case_list, event_date) -> int:
+def create_fta_entries(batch_case_list: List, event_date: str) -> int:
+    """Process that creates the entries and returns a count of total entries created."""
     entry_count = 0
     for case_number in batch_case_list:
         logger.info(f'Creating Batch FTA entry for: {case_number}')
@@ -108,7 +114,3 @@ def log_entries_created(entries_created: int) -> None:
     """Logs information about the number of entries created."""
     message = f'{entries_created} entries were created.'
     logger.info(message)
-
-
-if __name__ == '__main__':
-    logger.info(f'{__name__} run directly.')
