70fa9774ffa37b17adec06f71a1c19a2b28b79f3
Justin Kudela <justinkudela@gmail.com>
2023-03-05 06:09:48 -0500

    Renamed classes using AdminJudge instead of Hemmeter.

--- munientry/builders/workflows/hemmeter_dw_dialog.py
+++ munientry/builders/workflows/hemmeter_dw_dialog.py
@@ -1,4 +1,4 @@
-"""Builder for Hemmeter Digital Workflow Dialog."""
+"""Builder for Admin Judge Digital Workflow Dialog."""
 import datetime
 import os
 import shutil
@@ -8,7 +8,6 @@ from PyQt6.QtWidgets import QButtonGroup, QHeaderView, QTableWidgetItem, QWidget
 from PyQt6 import QtCore
 
 from munientry.builders import base_builders as base
-# from munientry.views.hemmeter_workflow_dialog_ui import Ui_HemmeterWorkflowDialog
 from munientry.views.admin_entries_workflow_dialog_ui import Ui_AdminEntriesWorkflowDialog
 from munientry.appsettings.paths import DW_HEMMETER, DW_APPROVED_DIR, DW_REJECTED_DIR
 from munientry.widgets.message_boxes import InfoBox, RequiredBox
@@ -19,28 +18,10 @@ COL_DECISION = 1
 COL_DATE = 2
 COL_TIME = 3
 ROW_HEIGHT = 50
+DATE_FORMAT = '%b %d, %Y'
+TIME_FORMAT = '%I:%M %p'
 
-
-class HemmeterWorkflowDialogViewModifier(base.BaseDialogViewModifier):
-    """View builder for Hemeter Workflow Dialog."""
-
-
-class HemmeterWorkflowDialogSignalConnector(base.BaseDialogSignalConnector):
-    """Signal connector for Hemmeter Workflow Dialog."""
-
-    def __init__(self, dialog):
-        self.dialog = dialog
-        self.connect_workflow_buttons()
-
-    def connect_workflow_buttons(self):
-        self.dialog.close_dialog_Button.released.connect(self.dialog.close)
-        self.dialog.open_entry_Button.released.connect(self.dialog.functions.open_entry)
-        self.dialog.complete_workflow_Button.released.connect(
-            self.dialog.functions.complete_workflow
-        )
-
-
-class RadioButtonWidget(QWidget):
+class DigitalWorkflowRadioButtonWidget(QWidget):
     """A Widget with two radio buttons for approving or rejecting a decision."""
 
     def __init__(self):
@@ -58,8 +39,27 @@ class RadioButtonWidget(QWidget):
         self.horizontalLayout.addWidget(self.rejected)
 
 
-class HemmeterWorkflowDialogSlotFunctions(base.BaseDialogSlotFunctions):
-    """Additional Functions for Hemmeter Workflow Dialog."""
+class AdminJudgeWorkflowDialogViewModifier(base.BaseDialogViewModifier):
+    """View builder for Admin Judge Workflow Dialog."""
+
+
+class AdminJudgeWorkflowDialogSignalConnector(base.BaseDialogSignalConnector):
+    """Signal connector for Admin Judge Workflow Dialog."""
+
+    def __init__(self, dialog):
+        self.dialog = dialog
+        self.connect_workflow_buttons()
+
+    def connect_workflow_buttons(self):
+        self.dialog.close_dialog_Button.released.connect(self.dialog.close)
+        self.dialog.open_entry_Button.released.connect(self.dialog.functions.open_entry)
+        self.dialog.complete_workflow_Button.released.connect(
+            self.dialog.functions.complete_workflow
+        )
+
+
+class AdminJudgeWorkflowDialogSlotFunctions(base.BaseDialogSlotFunctions):
+    """Additional Functions for Admin Judge Workflow Dialog."""
 
     def create_table_on_dialog_load(self):
         num_columns = 4
@@ -91,15 +91,15 @@ class HemmeterWorkflowDialogSlotFunctions(base.BaseDialogSlotFunctions):
             entry_path = os.path.join(self.dialog.entry_path, entry)
             entry_creation_time = os.path.getctime(entry_path)
             date_time_conversion = datetime.datetime.fromtimestamp(entry_creation_time)
-            date = date_time_conversion.strftime('%b %d, %Y')
-            time = date_time_conversion.strftime('%I:%M %p')
+            date = date_time_conversion.strftime(DATE_FORMAT)
+            time = date_time_conversion.strftime(TIME_FORMAT)
             self.create_entry_row(table, row, entry, date, time)
         table.setSortingEnabled(True)
 
     def create_entry_row(self, table_widget, row, entry_name, date, time):
         table_widget.insertRow(row)
         table_widget.setItem(row, COL_FILENAME, QTableWidgetItem(entry_name))
-        radio_button = RadioButtonWidget()
+        radio_button = DigitalWorkflowRadioButtonWidget()
         radio_button.buttonGroup.addButton((radio_button.approved))
         radio_button.buttonGroup.addButton((radio_button.rejected))
         table_widget.setCellWidget(row, COL_DECISION, radio_button)
@@ -107,11 +107,20 @@ class HemmeterWorkflowDialogSlotFunctions(base.BaseDialogSlotFunctions):
         table_widget.setItem(row, COL_TIME, QTableWidgetItem(time))
 
     def open_entry(self):
-        selected_entry_widget = self.dialog.entries_tableWidget.selectedItems()[0]
-        entry_name = selected_entry_widget.text()
-        document = f'{self.dialog.entry_path}/{entry_name}'
-        os.startfile(document)
-        logger.info(f'{document} opened in workflow.')
+        try:
+            selected_entry_widget = self.dialog.entries_tableWidget.selectedItems()[0]
+            entry_name = self.get_selected_entry_name(selected_entry_widget)
+            document = os.path.join(self.dialog.entry_path, entry_name)
+            os.startfile(document)
+            logger.info(f'{document} opened in workflow.')
+        except (IndexError, AttributeError):
+            InfoBox('No entry was selected to open.', 'No Entry Selected').exec()
+            logger.warning('No entry selected.')
+
+    def get_selected_entry_name(self, selected_entry_widget):
+        if selected_entry_widget is None:
+            raise AttributeError
+        return selected_entry_widget.text()
 
     def complete_workflow(self):
         """Loops through table rows and moves files if approved or rejected.
@@ -151,14 +160,13 @@ class HemmeterWorkflowDialogSlotFunctions(base.BaseDialogSlotFunctions):
                 table.removeRow(row)
 
 
-
 class AdminWorkflowDialog(base.BaseDialogBuilder, Ui_AdminEntriesWorkflowDialog):
     """Dialog builder class for Admin Entries Digital Workflow."""
 
     entry_path = ADMIN_ENTRY_PATH
-    _signal_connector = HemmeterWorkflowDialogSignalConnector
-    _slots = HemmeterWorkflowDialogSlotFunctions
-    _view_modifier = HemmeterWorkflowDialogViewModifier
+    _signal_connector = AdminJudgeWorkflowDialogSignalConnector
+    _slots = AdminJudgeWorkflowDialogSlotFunctions
+    _view_modifier = AdminJudgeWorkflowDialogViewModifier
     dialog_name = 'Admin Entries Digital Workflow'
 
     def __init__(self, parent=None):
