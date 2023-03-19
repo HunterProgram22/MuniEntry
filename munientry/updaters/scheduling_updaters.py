bfb4db36903b4f38c81185afa9c7834d4c6761f9
Justin Kudela <justinkudela@gmail.com>
2023-03-19 06:20:43 -0400

    Fixed logging updater issue.

--- munientry/updaters/scheduling_updaters.py
+++ munientry/updaters/scheduling_updaters.py
@@ -7,6 +7,7 @@ class SchedulingDialogCaseInformationUpdater(BaseModelUpdater):
 
     def __init__(self, dialog: 'QDialog') -> None:
         super().__init__(dialog)
+        self.set_party_information()
         self.set_defense_counsel_information()
         self.set_scheduling_dates()
 
