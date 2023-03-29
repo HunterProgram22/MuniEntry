0167544d800ea06e950d2a71e165d969ec2dd411
Justin Kudela <justinkudela@gmail.com>
2023-03-29 07:18:17 -0400

    Refactored base_checks. Passing flake8, pylint and mypy.
    Also removed checkfail and changed to warning for logging.

--- munientry/checkers/base_checks.py
+++ munientry/checkers/base_checks.py
@@ -1,4 +1,6 @@
-"""Module containing common information checks used on multiple dialogs."""
+"""Base module for all checks."""
+from typing import Any, Generator
+
 from loguru import logger
 from PyQt6.QtCore import QDate
 
@@ -8,7 +10,7 @@ from munientry.widgets.message_boxes import FAIL, PASS
 class BaseChecks(object):
     """Class for initializing Checks for Dialogs."""
 
-    conditions_list = []
+    conditions_list: list = []
 
     def __init__(self, dialog) -> None:
         self.dialog = dialog
@@ -16,8 +18,30 @@ class BaseChecks(object):
         self.today = QDate.currentDate()
 
     def perform_check_list(self) -> str:
-        for check_method in self.check_list:
-            if getattr(self, check_method)() == FAIL:
-                logger.checkfail(check_method)
-                return FAIL
+        """Loops through a list of checks and logs checks that fail.
+
+        The method is called prior to the entry creation process for a dialog. If any of the checks
+        fail it will show a message and either abort the entry creation process by returning FAIL or
+        show a message that allows the user to correct the failed check.
+        """
+        check_results = self.run_checks()
+        if FAIL in check_results:
+            self.log_failed_checks(check_results)
+            return FAIL
         return PASS
+
+    def run_checks(self) -> Generator[Any, None, None]:
+        """Returns a list of results of PASS or FAIL for each check in the check_list."""
+        return (getattr(self, check_method)() for check_method in self.check_list)
+
+    def log_failed_checks(self, check_results: Generator) -> None:
+        """Logs failed checks with warning."""
+        failed_check = next(
+            (
+                check
+                for check, check_result in zip(self.check_list, check_results)
+                if check_result == FAIL
+            ), None,
+        )
+        if failed_check:
+            logger.warning(failed_check)
