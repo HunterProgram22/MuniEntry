a24455671c6c6034d7486a16b513a9f10913cea8
Justin Kudela <justinkudela@gmail.com>
2023-04-17 07:00:24 -0400

    Removed unused class attributes for scheduling events.

--- munientry/models/scheduling_events.py
+++ munientry/models/scheduling_events.py
@@ -25,8 +25,6 @@ class Event(object):
     """Object for storing and packaging case event data to be inserted into the MuniEntryDB."""
 
     event_name_field: str
-    event_date_field: str
-    event_time_field: str
 
     def __init__(self, case_data_dict: dict):
         self.case_data_dict = case_data_dict
@@ -46,22 +44,11 @@ class Event(object):
             return self.event.get('type', 'None')
         return event_name_field
 
-    def load_event_time(self, event_time_field: str, event_name_field: str) -> str:
-        if event_time_field is None:
-            return '8:00 AM'
-        if event_time_field == 'pretrial_time':
-            return '3:00 PM'
-        if event_time_field == 'trial_time' and event_name_field == 'Jury Trial':
-            return '8:00 AM'
-        return self.case_data_dict.get(event_time_field, 'None')
-
 
 class FinalPretrialEvent(Event):
     """Event object for final pretrial court event."""
 
     event_name_field = 'Final Pretrial'
-    event_date_field = 'final_pretrial_date'
-    event_time_field = 'final_pretrial_time'
 
 
 class GeneralHearingEvent(Event):
@@ -72,33 +59,21 @@ class GeneralHearingEvent(Event):
     """
 
     event_name_field = 'hearing_type'
-    event_date_field = 'hearing_date'
-    event_time_field = 'hearing_time'
 
 
 class JuryTrialEvent(Event):
     """Event object for jury trial event."""
 
     event_name_field = 'Jury Trial'
-    event_date_field = 'jury_trial_date'
-    event_time_field = 'jury_trial_time'
 
 
 class TelephonePretrialEvent(Event):
     """Event object for telephone pretrial court event."""
 
     event_name_field = 'Telephone Pretrial'
-    event_date_field = 'pretrial_date'
-    event_time_field = 'pretrial_time'
 
 
 class TrialToCourtEvent(Event):
     """Event object for trial to court event."""
 
     event_name_field = 'Trial to Court'
-    event_date_field = 'trial_to_court_date'
-    event_time_field = 'trial_to_court_time'
-
-
-if __name__ == '__main__':
-    logger.info(f'{__name__} run directly.')
