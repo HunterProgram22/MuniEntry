b7fc3997ec598a9fb7dc5334971ab3b0eb2192c3
Justin Kudela <justinkudela@gmail.com>
2025-10-07 10:23:50 -0400

    Config file staff changes now also works for UI.

--- munientry/mainwindow/court_staff.py
+++ munientry/mainwindow/court_staff.py
@@ -62,6 +62,7 @@ class CourtStaffManager(object):
             self.mw.bond_intake_officer_radio_btn,
         ]
         self.court_staff_buttons_dict: dict = self.create_court_staff_buttons_dict()
+        self.update_button_labels()
 
     def set_person_stack_widget(self) -> None:
         stack_mapping = {
@@ -100,6 +101,64 @@ class CourtStaffManager(object):
         first_name = staff.get(f'{prefix}_first', 'Unknown')
         last_name = staff.get(f'{prefix}_last', 'Unknown')
         return JudicialOfficer(first_name, last_name, role)
+
+    def update_button_labels(self) -> None:
+        """Update all radio button labels with names from config."""
+        # Judges
+        judge_1 = self._get_staff_member('judge_1', JUDGE)
+        judge_2 = self._get_staff_member('judge_2', JUDGE)
+        self.mw.judge_1_radio_btn.setText(f'Judge {judge_1.last_name}')
+        self.mw.judge_2_radio_btn.setText(f'Judge {judge_2.last_name}')
+
+        # Magistrates
+        mag_1 = self._get_staff_member('magistrate_1', MAGISTRATE)
+        mag_2 = self._get_staff_member('magistrate_2', MAGISTRATE)
+        mag_3 = self._get_staff_member('magistrate_3', MAGISTRATE)
+        self.mw.mag_1_radio_btn.setText(f'Magistrate {mag_1.last_name}')
+        self.mw.mag_2_radio_btn.setText(f'Magistrate {mag_2.last_name}')
+        self.mw.mag_3_radio_btn.setText(f'Magistrate {mag_3.last_name}')
+
+        # Assignment Commissioners
+        assn_1 = self._get_staff_member('assignment_comm_1', ASSN_COMM)
+        assn_2 = self._get_staff_member('assignment_comm_2', ASSN_COMM)
+        self.mw.assn_comm_1_radio_btn.setText(f'{assn_1.first_name} {assn_1.last_name}')
+        self.mw.assn_comm_2_radio_btn.setText(f'{assn_2.first_name} {assn_2.last_name}')
+
+        # Admin Assignment Commissioners
+        self.mw.assn_comm_1_admin_radio_btn.setText(f'Assignment Commissioner {assn_1.last_name}')
+        self.mw.assn_comm_2_admin_radio_btn.setText(f'Assignment Commissioner {assn_2.last_name}')
+
+        # Admin Court Administrator and Jury Commissioner
+        court_admin = self._get_staff_member('court_admin', CT_ADMIN)
+        jury_comm = self._get_staff_member('jury_comm', JURY)
+        self.mw.court_admin_admin_radio_btn.setText(f'Court Administrator {court_admin.last_name}')
+        self.mw.jury_comm_1_admin_radio_btn.setText(f'Jury Commissioner {jury_comm.last_name}')
+
+        # Admin Magistrates
+        self.mw.mag_1_admin_radio_btn.setText(f'Magistrate {mag_1.last_name}')
+        self.mw.mag_2_admin_radio_btn.setText(f'Magistrate {mag_2.last_name}')
+        self.mw.mag_3_admin_radio_btn.setText(f'Magistrate {mag_3.last_name}')
+
+        # Probation Staff
+        chief = self._get_staff_member('chief_cco', CHIEF_CCO)
+        deputy_chief = self._get_staff_member('deputy_chief_cco', DEP_CCCO)
+        cco_1 = self._get_staff_member('cco_1', CCO)
+        cco_2 = self._get_staff_member('cco_2', CCO)
+        cco_3 = self._get_staff_member('cco_3', CCO)
+        cco_4 = self._get_staff_member('cco_4', CCO)
+        cco_5 = self._get_staff_member('cco_5', CCO)
+        bond_officer = self._get_staff_member('bond_intake_officer', BOND_CCO)
+
+        self.mw.chief_prob_officer_radio_btn.setText(f'Chief {chief.first_name} {chief.last_name}')
+        self.mw.dep_chief_prob_officer_radio_btn.setText(
+            f'Deputy Chief {deputy_chief.first_name} {deputy_chief.last_name}')
+        self.mw.prob_officer_1_radio_btn.setText(f'Officer {cco_1.first_name} {cco_1.last_name}')
+        self.mw.prob_officer_2_radio_btn.setText(f'Officer {cco_2.first_name} {cco_2.last_name}')
+        self.mw.prob_officer_3_radio_btn.setText(f'Officer {cco_3.first_name} {cco_3.last_name}')
+        self.mw.prob_officer_4_radio_btn.setText(f'Officer {cco_4.first_name} {cco_4.last_name}')
+        self.mw.prob_officer_5_radio_btn.setText(f'Officer {cco_5.first_name} {cco_5.last_name}')
+        self.mw.bond_intake_officer_radio_btn.setText(
+            f'Pretrial Officer {bond_officer.first_name} {bond_officer.last_name}')
 
     def create_court_staff_buttons_dict(self) -> dict:
         return {
